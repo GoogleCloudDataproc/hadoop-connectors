74834f20133cae78c04d3258c327374fc7eb90bd
Igor Dvorzhak <idv@google.com>
2019-06-17 22:24:59 -0700

    Test improvements

--- gcs/src/test/java/com/google/cloud/hadoop/fs/gcs/InMemoryGlobberFileSystemTest.java
+++ gcs/src/test/java/com/google/cloud/hadoop/fs/gcs/InMemoryGlobberFileSystemTest.java
@@ -14,70 +14,36 @@
 
 package com.google.cloud.hadoop.fs.gcs;
 
-import static com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystemTestBase.loadConfig;
 import static com.google.common.truth.Truth.assertThat;
 import static org.junit.Assert.assertThrows;
 
-import com.google.cloud.hadoop.gcsio.GoogleCloudStorageFileSystemIntegrationHelper;
-import com.google.cloud.hadoop.gcsio.StorageResourceId;
+import com.google.common.collect.ImmutableSet;
 import java.io.FileNotFoundException;
-import java.net.URI;
-import java.util.Arrays;
-import java.util.List;
-import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-/** Integration tests for {@link InMemoryGlobberFileSystem} class. */
+/** Unit tests for {@link InMemoryGlobberFileSystem} class. */
 @RunWith(JUnit4.class)
 public class InMemoryGlobberFileSystemTest {
-  private static GoogleCloudStorageFileSystemIntegrationHelper gcsFsIHelper;
-
-  @BeforeClass
-  public static void beforeClass() throws Exception {
-    gcsFsIHelper =
-        GoogleCloudStorageFileSystemIntegrationHelper.create(
-            GoogleHadoopFileSystemIntegrationHelper.APP_NAME);
-    gcsFsIHelper.beforeAllTests();
-  }
-
-  @AfterClass
-  public static void afterClass() {
-    gcsFsIHelper.afterAllTests();
-  }
 
   @Test
   public void testGetFileStatus_shouldReturnExceptionForNotExistingFile() throws Exception {
-    StorageResourceId testFile =
-        new StorageResourceId(
-            gcsFsIHelper.sharedBucketName1, "InMemoryGlobberFileSystem_testGetNullFileStatus");
-    GoogleHadoopFileSystem ghfs =
-        GoogleHadoopFileSystemIntegrationHelper.createGhfs(
-            testFile.toString(), GoogleHadoopFileSystemIntegrationHelper.getTestConfig());
-    String testContent = "test content";
-    gcsFsIHelper.writeTextFile(testFile.getBucketName(), testFile.getObjectName(), testContent);
-    URI fileUri =
-        new URI(String.format("gs://%s/%s", testFile.getBucketName(), testFile.getObjectName()));
+    GoogleHadoopFileSystem ghfs = new InMemoryGoogleHadoopFileSystem();
 
-    Path filePath = new Path(fileUri);
-    FileStatus status = ghfs.getFileStatus(filePath);
-    List<FileStatus> fileStatuses = Arrays.asList(status);
     FileSystem helperFileSystem =
         InMemoryGlobberFileSystem.createInstance(
-            loadConfig(), ghfs.getWorkingDirectory(), fileStatuses);
-    ghfs.delete(filePath, true);
+            new Configuration(), ghfs.getWorkingDirectory(), ImmutableSet.of());
 
     FileNotFoundException e =
         assertThrows(
             FileNotFoundException.class,
             () -> helperFileSystem.getFileStatus(ghfs.getWorkingDirectory()));
 
-    assertThat(e.getLocalizedMessage())
+    assertThat(e)
+        .hasMessageThat()
         .startsWith(
             String.format(
                 "Path '%s' (qualified: '%s') does not exist.",
