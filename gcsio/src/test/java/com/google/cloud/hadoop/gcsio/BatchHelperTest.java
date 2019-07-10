741d41ccf26815c38eb406a05dcfe666aacbfda4
Igor Dvorzhak <idmedb@gmail.com>
2019-07-09 23:25:50 -0700

    Address review comments

--- gcsio/src/test/java/com/google/cloud/hadoop/gcsio/BatchHelperTest.java
+++ gcsio/src/test/java/com/google/cloud/hadoop/gcsio/BatchHelperTest.java
@@ -20,13 +20,13 @@ import static com.google.cloud.hadoop.gcsio.GoogleCloudStorageTestUtils.BUCKET_N
 import static com.google.cloud.hadoop.gcsio.GoogleCloudStorageTestUtils.JSON_FACTORY;
 import static com.google.cloud.hadoop.gcsio.GoogleCloudStorageTestUtils.OBJECT_NAME;
 import static com.google.cloud.hadoop.gcsio.GoogleCloudStorageTestUtils.metadataResponse;
+import static com.google.cloud.hadoop.gcsio.TrackingHttpRequestInitializer.batchRequestString;
+import static com.google.cloud.hadoop.gcsio.TrackingHttpRequestInitializer.getRequestString;
 import static com.google.common.truth.Truth.assertThat;
 
 import com.google.api.client.googleapis.batch.json.JsonBatchCallback;
 import com.google.api.client.googleapis.json.GoogleJsonError;
-import com.google.api.client.http.GenericUrl;
 import com.google.api.client.http.HttpHeaders;
-import com.google.api.client.http.HttpRequest;
 import com.google.api.client.testing.http.MockHttpTransport;
 import com.google.api.client.util.DateTime;
 import com.google.api.services.storage.Storage;
@@ -34,7 +34,6 @@ import com.google.api.services.storage.model.StorageObject;
 import java.io.IOException;
 import java.math.BigInteger;
 import java.util.Date;
-import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -52,6 +51,7 @@ public class BatchHelperTest {
 
   @Test
   public void allRequestsAreSentInSingleBatch() throws IOException {
+    // 1. Prepare test data
     String objectName1 = OBJECT_NAME + "-01";
     String objectName2 = OBJECT_NAME + "-02";
 
@@ -63,7 +63,6 @@ public class BatchHelperTest {
             .setGeneration(1L)
             .setMetageneration(1L)
             .setUpdated(new DateTime(new Date()));
-
     StorageObject object2 =
         new StorageObject()
             .setBucket(BUCKET_NAME)
@@ -73,10 +72,12 @@ public class BatchHelperTest {
             .setMetageneration(3L)
             .setUpdated(new DateTime(new Date()));
 
+    // 2. Configure mock HTTP transport with test request responses
     MockHttpTransport transport =
         GoogleCloudStorageTestUtils.mockBatchTransport(
             /* requestsPerBatch= */ 2, metadataResponse(object1), metadataResponse(object2));
 
+    // 3. Configure BatchHelper with mocked HTTP transport
     TrackingHttpRequestInitializer httpRequestInitializer = new TrackingHttpRequestInitializer();
     Storage storage = new Storage(transport, JSON_FACTORY, httpRequestInitializer);
     BatchHelper batchHelper =
@@ -87,17 +88,30 @@ public class BatchHelperTest {
             /* totalRequests= */ 2,
             /* maxThreads= */ 0);
 
+    // 4. Queue 1st GET request to BatchHelper
     batchHelper.queue(storage.objects().get(BUCKET_NAME, objectName1), assertCallback(object1));
+
+    // 5. Validate that no requests were sent after 1st request were queued
+    assertThat(httpRequestInitializer.getAllRequestStrings()).isEmpty();
+
+    // 6. Queue 2nd GET request to BatchHelper
     batchHelper.queue(storage.objects().get(BUCKET_NAME, objectName2), assertCallback(object2));
 
+    // 7. Validate that 1 batch request consisting of 2 GET requests was sent
+    assertThat(httpRequestInitializer.getAllRequestStrings())
+        .containsExactly(
+            batchRequestString(),
+            getRequestString(BUCKET_NAME, objectName1),
+            getRequestString(BUCKET_NAME, objectName2));
+
+    // 8. Reset httpRequestInitializer before validating `flush` method
+    httpRequestInitializer.reset();
+
+    // 9. Call `flush` at the end.
     batchHelper.flush();
 
-    List<HttpRequest> requests = httpRequestInitializer.getAllRequests();
-    assertThat(requests).hasSize(3);
-
-    assertThat(requests.get(0).getRequestMethod()).isEqualTo("POST");
-    assertThat(requests.get(0).getUrl())
-        .isEqualTo(new GenericUrl("https://www.googleapis.com/storage/v1/batch"));
+    // 10. Validate that `flush` method call is noop if all requests were sent already
+    assertThat(httpRequestInitializer.getAllRequestStrings()).isEmpty();
   }
 
   private JsonBatchCallback<StorageObject> assertCallback(StorageObject expectedObject) {
