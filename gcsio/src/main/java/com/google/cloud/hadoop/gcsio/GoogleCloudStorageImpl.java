72eb42402c5d2130c3fe24ab5449eba9e13c31d9
Igor Dvorzhak <idv@google.com>
2020-03-16 17:40:59 -0700

    Improve exception handling

--- gcsio/src/main/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageImpl.java
+++ gcsio/src/main/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageImpl.java
@@ -450,8 +450,7 @@ public class GoogleCloudStorageImpl implements GoogleCloudStorage {
           new GoogleCloudStorageGrpcWriteChannel(
               backgroundTasksThreadPool,
               gcsGrpcStub,
-              resourceId.getBucketName(),
-              resourceId.getObjectName(),
+              resourceId,
               storageOptions.getWriteChannelOptions(),
               writeConditions,
               requesterPaysProject,
