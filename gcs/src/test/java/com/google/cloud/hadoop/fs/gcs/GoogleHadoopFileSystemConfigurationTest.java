bc7c0a4f52d39a4c006f6ff0778f23e91e1aa98b
BrandonY <brandony@gmail.com>
2020-04-23 19:04:18 -0700

    Balance gRPC connections with bpool algorithm. (#357)
    Co-authored-by: Igor Dvorzhak <idv@google.com>

--- gcs/src/test/java/com/google/cloud/hadoop/fs/gcs/GoogleHadoopFileSystemConfigurationTest.java
+++ gcs/src/test/java/com/google/cloud/hadoop/fs/gcs/GoogleHadoopFileSystemConfigurationTest.java
@@ -105,7 +105,7 @@ public class GoogleHadoopFileSystemConfigurationTest {
           put("fs.gs.encryption.key.hash", null);
           put("fs.gs.grpc.enable", false);
           put("fs.gs.grpc.checksums.enable", false);
-          put("fs.gs.grpc.server.address", "storage.googleapis.com:443");
+          put("fs.gs.grpc.server.address", null);
         }
       };
 
