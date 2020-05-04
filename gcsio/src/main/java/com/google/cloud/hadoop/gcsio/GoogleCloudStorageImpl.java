hongyegong/bigdata-interop

Explicitly set the executor for the gRPC stub.
This doesn't seem to affect performance much, but the default behavior
is creating a new executor for no good reason. Better to reuse the
existing thread pool.

Change-Id: If32b856e8eae5a7e695503c6848b2c41a1f2e205

gcsio/src/main/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageImpl.java
@@ -265,7 +265,8 @@ public class GoogleCloudStorageImpl implements GoogleCloudStorage {
         StorageGrpc.newStub(
             GoogleDefaultChannelBuilder.forTarget(GRPC_TARGET)
                 .defaultServiceConfig(serviceConfig)
-                .build());
+                .build())
+            .withExecutor(threadPool);
   }
 
   private Map<String, Object> getGrpcServiceConfig(GoogleCloudStorageReadOptions readOptions) {

GoogleCloudDataproc/hadoop-connectors

gcsio/src/main/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageImpl.java
@@ -265,7 +265,8 @@ public class GoogleCloudStorageImpl implements GoogleCloudStorage {
         StorageGrpc.newStub(
             GoogleDefaultChannelBuilder.forTarget(GRPC_TARGET)
                 .defaultServiceConfig(serviceConfig)
-                .build());
+                .build())
+            .withExecutor(backgroundTasksThreadPool);
   }
 
   private Map<String, Object> getGrpcServiceConfig(GoogleCloudStorageReadOptions readOptions) {