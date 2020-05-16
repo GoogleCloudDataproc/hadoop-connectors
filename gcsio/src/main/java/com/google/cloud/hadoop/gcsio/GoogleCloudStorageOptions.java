ca36c4e5193d372353ad65893588ac3ad526fc3d
hongyegong <hg1068@nyu.edu>
2020-05-15 21:42:35 -0700

    remove autowindow settings from connector (#386)

--- gcsio/src/main/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageOptions.java
+++ gcsio/src/main/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageOptions.java
@@ -35,9 +35,6 @@ public abstract class GoogleCloudStorageOptions {
   /** Default setting for enabling use of GCS gRPC API. */
   public static final boolean ENABLE_GRPC_DEFAULT = false;
 
-  /** Default setting for enabling gRPC auto flow-control window. */
-  public static final boolean ENABLE_GRPC_AUTO_WINDOW_DEFAULT = false;
-
   /** Default root URL for Cloud Storage API endpoint. */
   public static final String STORAGE_ROOT_URL_DEFAULT = Storage.DEFAULT_ROOT_URL;
 
@@ -100,7 +97,6 @@ public abstract class GoogleCloudStorageOptions {
   public static Builder builder() {
     return new AutoValue_GoogleCloudStorageOptions.Builder()
         .setGrpcEnabled(ENABLE_GRPC_DEFAULT)
-        .setGrpcAutoWindowEnabled(ENABLE_GRPC_AUTO_WINDOW_DEFAULT)
         .setStorageRootUrl(STORAGE_ROOT_URL_DEFAULT)
         .setStorageServicePath(STORAGE_SERVICE_PATH_DEFAULT)
         .setAutoRepairImplicitDirectoriesEnabled(AUTO_REPAIR_IMPLICIT_DIRECTORIES_DEFAULT)
@@ -125,8 +121,6 @@ public abstract class GoogleCloudStorageOptions {
   }
 
   public abstract boolean isGrpcEnabled();
-
-  public abstract boolean isGrpcAutoWindowEnabled();
 
   public abstract String getStorageRootUrl();
 
@@ -205,8 +199,6 @@ public abstract class GoogleCloudStorageOptions {
   public abstract static class Builder {
 
     public abstract Builder setGrpcEnabled(boolean grpcEnabled);
-
-    public abstract Builder setGrpcAutoWindowEnabled(boolean grpcAutoWindowEnabled);
 
     public abstract Builder setStorageRootUrl(String rootUrl);
 
