GoogleCloudDataproc/initialization-actions

grpc version bump and minor fix (#403)

gcsio/src/main/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageExceptions.java
@@ -84,7 +84,10 @@ public final class GoogleCloudStorageExceptions {
 
   public static GoogleJsonResponseException createJsonResponseException(
       GoogleJsonError e, HttpHeaders responseHeaders) {
-    return new GoogleJsonResponseException(
-        new HttpResponseException.Builder(e.getCode(), e.getMessage(), responseHeaders), e);
+    if (e != null) {
+      return new GoogleJsonResponseException(
+          new HttpResponseException.Builder(e.getCode(), e.getMessage(), responseHeaders), e);
+    }
+    return null;
   }
 }

GoogleCloudDataproc/hadoop-connectors

gcsio/src/main/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageExceptions.java
@@ -83,7 +83,10 @@ public final class GoogleCloudStorageExceptions {
 
   public static GoogleJsonResponseException createJsonResponseException(
       GoogleJsonError e, HttpHeaders responseHeaders) {
-    return new GoogleJsonResponseException(
-        new GoogleJsonResponseException.Builder(e.getCode(), e.getMessage(), responseHeaders), e);
+    if (e != null) {
+      return new GoogleJsonResponseException(
+          new GoogleJsonResponseException.Builder(e.getCode(), e.getMessage(), responseHeaders), e);
+    }
+    return null;
   }
 }