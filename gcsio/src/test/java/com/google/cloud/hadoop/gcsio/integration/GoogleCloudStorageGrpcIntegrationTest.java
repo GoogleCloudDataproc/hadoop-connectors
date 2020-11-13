65db9a4d0d96858ba6c97b6443d1c060fdbe00af
hongyegong <hg1068@nyu.edu>
2020-11-12 16:19:34 -0800

    refactoring

--- gcsio/src/test/java/com/google/cloud/hadoop/gcsio/integration/GoogleCloudStorageGrpcIntegrationTest.java
+++ gcsio/src/test/java/com/google/cloud/hadoop/gcsio/integration/GoogleCloudStorageGrpcIntegrationTest.java
@@ -9,16 +9,15 @@ import static org.junit.Assert.assertThrows;
 import com.google.cloud.hadoop.gcsio.GoogleCloudStorage;
 import com.google.cloud.hadoop.gcsio.GoogleCloudStorageImpl;
 import com.google.cloud.hadoop.gcsio.GoogleCloudStorageItemInfo;
+import com.google.cloud.hadoop.gcsio.ListObjectOptions;
 import com.google.cloud.hadoop.gcsio.StorageResourceId;
 import com.google.cloud.hadoop.gcsio.ThrottledGoogleCloudStorage;
 import com.google.cloud.hadoop.gcsio.ThrottledGoogleCloudStorage.StorageOperation;
-import com.google.cloud.hadoop.gcsio.integration.GoogleCloudStorageTestHelper.TestBucketHelper;
-import com.google.common.base.Supplier;
-import com.google.common.base.Suppliers;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
-import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+import com.google.common.flogger.GoogleLogger;
 import com.google.common.util.concurrent.RateLimiter;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -27,18 +26,16 @@ import java.nio.channels.ClosedChannelException;
 import java.nio.channels.SeekableByteChannel;
 import java.util.Arrays;
 import java.util.EnumSet;
+import java.util.List;
 import org.junit.AfterClass;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class GoogleCloudStorageGrpcIntegrationTest {
 
-  // This prefix will be replaced by the prefix used in other gcs io integrate tests once it's
-  // whitelisted by GCS to access gRPC API.
-  private static final String BUCKET_NAME_PREFIX = "gcs-grpc-team-dataproc-it";
-
-  private static final Supplier<TestBucketHelper> BUCKET_HELPER =
-      Suppliers.memoize(() -> new TestBucketHelper(BUCKET_NAME_PREFIX));
+  // Prefix this name with the prefix used in other gcs io integrate tests once it's whitelisted by
+  // GCS to access gRPC API.
+  private static final String BUCKET_NAME = "gcs-grpc-team-dataproc-it";
 
   private static final LoadingCache<GoogleCloudStorage, String> SHARED_BUCKETS =
       CacheBuilder.newBuilder()
@@ -46,46 +43,51 @@ public class GoogleCloudStorageGrpcIntegrationTest {
               new CacheLoader<GoogleCloudStorage, String>() {
                 @Override
                 public String load(GoogleCloudStorage gcs) throws Exception {
-                  return BUCKET_HELPER.get().getUniqueBucketName("shared") + "_" + gcs.hashCode();
+                  return BUCKET_NAME;
                 }
               });
 
-  private final GoogleCloudStorage rawStorage;
+  private static final GoogleCloudStorage rawStorage = getRawStorage();
 
-  public GoogleCloudStorageGrpcIntegrationTest() throws IOException {
-    GoogleCloudStorage gcs =
-        new ThrottledGoogleCloudStorage(
-            // Allow 2 create or delete bucket operation every second. This will hit rate limits,
-            // but GCS now has back-offs implemented for bucket operations.
-            RateLimiter.create(2),
-            new GoogleCloudStorageImpl(
-                GoogleCloudStorageTestHelper.getStandardOptionBuilder()
-                    .setGrpcEnabled(true)
-                    .build(),
-                GoogleCloudStorageTestHelper.getCredential()),
-            EnumSet.of(StorageOperation.DELETE_BUCKETS, StorageOperation.CREATE_BUCKET));
-    this.rawStorage = gcs;
+  private static GoogleCloudStorage getRawStorage() {
+    try {
+      return new ThrottledGoogleCloudStorage(
+          // Allow 2 create or delete bucket operation every second. This will hit rate limits,
+          // but GCS now has back-offs implemented for bucket operations.
+          RateLimiter.create(2),
+          new GoogleCloudStorageImpl(
+              GoogleCloudStorageTestHelper.getStandardOptionBuilder().setGrpcEnabled(true).build(),
+              GoogleCloudStorageTestHelper.getCredential()),
+          EnumSet.of(StorageOperation.DELETE_BUCKETS, StorageOperation.CREATE_BUCKET));
+    } catch (IOException e) {
+      GoogleLogger logger = GoogleLogger.forEnclosingClass();
+      logger.atWarning().withCause(e).log(
+          "Caught exception during GCS (%s) buckets creation", BUCKET_NAME);
+      return null;
+    }
   }
 
-  @Before
-  public void setUp() throws IOException {
-    rawStorage.createBucket(getSharedBucketName());
+  @BeforeClass
+  public static void createBuckets() throws IOException {
+    rawStorage.createBucket(BUCKET_NAME);
   }
 
   @AfterClass
   public static void cleanupBuckets() throws IOException {
-    BUCKET_HELPER.get().cleanup(Iterables.getLast(SHARED_BUCKETS.asMap().keySet()));
-  }
+    // randomize buckets order in case concurrent clean ups are running
+    List<GoogleCloudStorageItemInfo> objectsToDelete =
+        rawStorage.listObjectInfo(
+            BUCKET_NAME, /* objectNamePrefix= */ null, ListObjectOptions.DEFAULT_FLAT_LIST);
 
-  private String getSharedBucketName() {
-    return SHARED_BUCKETS.getUnchecked(rawStorage);
+    rawStorage.deleteObjects(
+        Lists.transform(objectsToDelete, GoogleCloudStorageItemInfo::getResourceId));
+    rawStorage.deleteBuckets(Arrays.asList(BUCKET_NAME));
   }
 
   @Test
   public void testCreateObject() throws IOException {
-    String bucketName = getSharedBucketName();
-
-    StorageResourceId objectToCreate = new StorageResourceId(bucketName, "testCreateObject_Object");
+    StorageResourceId objectToCreate =
+        new StorageResourceId(BUCKET_NAME, "testCreateObject_Object");
     byte[] objectBytes = writeObject(rawStorage, objectToCreate, /* objectSize= */ 512);
 
     assertObjectContent(rawStorage, objectToCreate, objectBytes);
@@ -93,10 +95,8 @@ public class GoogleCloudStorageGrpcIntegrationTest {
 
   @Test
   public void testCreateExistingObject() throws IOException {
-    String bucketName = getSharedBucketName();
-
     StorageResourceId objectToCreate =
-        new StorageResourceId(bucketName, "testCreateExistingObject_Object");
+        new StorageResourceId(BUCKET_NAME, "testCreateExistingObject_Object");
     writeObject(rawStorage, objectToCreate, /* objectSize= */ 128);
 
     GoogleCloudStorageItemInfo createdItemInfo = rawStorage.getItemInfo(objectToCreate);
@@ -113,10 +113,8 @@ public class GoogleCloudStorageGrpcIntegrationTest {
 
   @Test
   public void testCreateEmptyObject() throws IOException {
-    String bucketName = getSharedBucketName();
-
     StorageResourceId objectToCreate =
-        new StorageResourceId(bucketName, "testCreateEmptyObject_Object");
+        new StorageResourceId(BUCKET_NAME, "testCreateEmptyObject_Object");
 
     rawStorage.createEmptyObject(objectToCreate);
 
@@ -128,10 +126,8 @@ public class GoogleCloudStorageGrpcIntegrationTest {
 
   @Test
   public void testCreateInvalidObject() {
-    String bucketName = getSharedBucketName();
-
     StorageResourceId objectToCreate =
-        new StorageResourceId(bucketName, "testCreateInvalidObject_InvalidObject\n");
+        new StorageResourceId(BUCKET_NAME, "testCreateInvalidObject_InvalidObject\n");
 
     assertThrows(
         IOException.class, () -> writeObject(rawStorage, objectToCreate, /* objectSize= */ 10));
@@ -139,9 +135,7 @@ public class GoogleCloudStorageGrpcIntegrationTest {
 
   @Test
   public void testOpen() throws IOException {
-    String bucketName = getSharedBucketName();
-
-    StorageResourceId objectToCreate = new StorageResourceId(bucketName, "testOpen_Object");
+    StorageResourceId objectToCreate = new StorageResourceId(BUCKET_NAME, "testOpen_Object");
     byte[] objectBytes = writeObject(rawStorage, objectToCreate, /* objectSize= */ 100);
 
     assertObjectContent(rawStorage, objectToCreate, objectBytes);
@@ -149,18 +143,15 @@ public class GoogleCloudStorageGrpcIntegrationTest {
 
   @Test
   public void testOpenNonExistentItem() {
-    String bucketName = getSharedBucketName();
-
     assertThrows(
         FileNotFoundException.class,
-        () -> rawStorage.open(new StorageResourceId(bucketName, "testOpenNonExistentItem_Object")));
+        () ->
+            rawStorage.open(new StorageResourceId(BUCKET_NAME, "testOpenNonExistentItem_Object")));
   }
 
   @Test
   public void testOpenEmptyObject() throws IOException {
-    String bucketName = getSharedBucketName();
-
-    StorageResourceId resourceId = new StorageResourceId(bucketName, "testOpenEmptyObject_Object");
+    StorageResourceId resourceId = new StorageResourceId(BUCKET_NAME, "testOpenEmptyObject_Object");
     rawStorage.createEmptyObject(resourceId);
 
     assertObjectContent(rawStorage, resourceId, new byte[0]);
@@ -168,8 +159,7 @@ public class GoogleCloudStorageGrpcIntegrationTest {
 
   @Test
   public void testOpenLargeObject() throws IOException {
-    String bucketName = getSharedBucketName();
-    StorageResourceId resourceId = new StorageResourceId(bucketName, "testOpenLargeObject_Object");
+    StorageResourceId resourceId = new StorageResourceId(BUCKET_NAME, "testOpenLargeObject_Object");
 
     int partitionsCount = 50;
     byte[] partition =
@@ -183,10 +173,8 @@ public class GoogleCloudStorageGrpcIntegrationTest {
     final int segmentSize = 553;
     final int segmentCount = 5;
 
-    String bucketName = getSharedBucketName();
-
     StorageResourceId resourceId =
-        new StorageResourceId(bucketName, "testReadPartialObjects_Object");
+        new StorageResourceId(BUCKET_NAME, "testReadPartialObjects_Object");
     byte[] data = writeObject(rawStorage, resourceId, /* objectSize= */ segmentCount * segmentSize);
 
     byte[][] readSegments = new byte[segmentCount][segmentSize];
@@ -210,10 +198,9 @@ public class GoogleCloudStorageGrpcIntegrationTest {
   @Test
   public void testChannelClosedException() throws IOException {
     final int totalBytes = 1200;
-    String bucketName = getSharedBucketName();
 
     StorageResourceId resourceId =
-        new StorageResourceId(bucketName, "testChannelClosedException_Object");
+        new StorageResourceId(BUCKET_NAME, "testChannelClosedException_Object");
     writeObject(rawStorage, resourceId, /* objectSize= */ totalBytes);
 
     byte[] readArray = new byte[totalBytes];
