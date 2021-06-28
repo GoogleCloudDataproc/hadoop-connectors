064b784ca5114db88e2d312e9d3fdb705f62f78e
Prashanth Sagar <mprashanthsagar@gmail.com>
2021-06-28 23:26:23 +0530

    Fix resumable upload from offset of last chunk (#577)

--- gcsio/src/main/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageGrpcWriteChannel.java
+++ gcsio/src/main/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageGrpcWriteChannel.java
@@ -67,13 +67,6 @@ public final class GoogleCloudStorageGrpcWriteChannel
   private static final Duration START_RESUMABLE_WRITE_TIMEOUT = Duration.ofMinutes(1);
   private static final Duration QUERY_WRITE_STATUS_TIMEOUT = Duration.ofMinutes(1);
 
-  // Number of insert requests to retain, in case we need to rewind and resume an upload. Using too
-  // small of a number could risk being unable to resume the write if the resume point is an
-  // already-discarded buffer; and setting the value too high wastes RAM. Note: We could have a
-  // more complex implementation that periodically queries the service to find out the last
-  // committed offset, to determine what's safe to discard, but that would also impose a performance
-  // penalty.
-  private static final int NUMBER_OF_REQUESTS_TO_RETAIN = 5;
   // A set that defines all transient errors on which retry can be attempted.
   private static final ImmutableSet<Status.Code> TRANSIENT_ERRORS =
       ImmutableSet.of(
@@ -232,7 +225,7 @@ public final class GoogleCloudStorageGrpcWriteChannel
               ByteString.readFrom(
                   ByteStreams.limit(pipeSource, MAX_BYTES_PER_MESSAGE), MAX_BYTES_PER_MESSAGE);
           dataChunkMap.put(writeOffset, data);
-          if (dataChunkMap.size() >= NUMBER_OF_REQUESTS_TO_RETAIN) {
+          if (dataChunkMap.size() >= channelOptions.getNumberOfBufferedRequests()) {
             dataChunkMap.remove(dataChunkMap.firstKey());
           }
           insertRequest = buildInsertRequest(writeOffset, data, false);
@@ -320,7 +313,8 @@ public final class GoogleCloudStorageGrpcWriteChannel
       InsertObjectRequest request = null;
       if (dataChunkMap.size() > 0 && dataChunkMap.firstKey() <= writeOffset) {
         for (Map.Entry<Long, ByteString> entry : dataChunkMap.entrySet()) {
-          if (entry.getKey() + entry.getValue().size() > writeOffset) {
+          if (entry.getKey() + entry.getValue().size() > writeOffset
+              || entry.getKey() == writeOffset) {
             Long writeOffsetToResume = entry.getKey();
             ByteString chunkData = entry.getValue();
             request = buildInsertRequest(writeOffsetToResume, chunkData, true);
