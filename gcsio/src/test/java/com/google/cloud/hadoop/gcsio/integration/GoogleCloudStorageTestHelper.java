d2919a931544602275aef52b9ab9a828a44380c6
Zamil Majdy <zamil.majdy@databricks.com>
2021-07-27 18:38:00 +0200

    Prevent on using dot in a bucket name when running integration tests

--- gcsio/src/test/java/com/google/cloud/hadoop/gcsio/integration/GoogleCloudStorageTestHelper.java
+++ gcsio/src/test/java/com/google/cloud/hadoop/gcsio/integration/GoogleCloudStorageTestHelper.java
@@ -245,7 +245,8 @@ public class GoogleCloudStorageTestHelper {
     }
 
     private static String makeBucketName(String prefix) {
-      String username = System.getProperty("user.name", "unknown").replace("-", "");
+      String username = System.getProperty("user.name", "unknown")
+          .replace("-", "").replace(".", "");
       username = username.substring(0, Math.min(username.length(), 10));
       String uuidSuffix = UUID.randomUUID().toString().substring(0, 8);
       return prefix + DELIMITER + username + DELIMITER + uuidSuffix;
