a497e310b2b28180de95e38281431b4412dc9675
BhagyaaShreeS <bsathyamurthy@whirldatascience.com>
2021-09-07 16:59:06 +0530

    commit as a part of 8e7ba1ad235f55183e86dca725463b11153be569

--- gcs/src/main/java/com/google/cloud/hadoop/fs/gcs/GoogleHadoopFileSystemConfiguration.java
+++ gcs/src/main/java/com/google/cloud/hadoop/fs/gcs/GoogleHadoopFileSystemConfiguration.java
@@ -427,9 +427,7 @@ public class GoogleHadoopFileSystemConfiguration {
 
   // TODO(b/120887495): This @VisibleForTesting annotation was being ignored by prod code.
   // Please check that removing it is correct, and remove this comment along with it.
-  //Bhagyaa-s
   @VisibleForTesting
-  //Bhagyaa-e
   public static GoogleCloudStorageFileSystemOptions.Builder getGcsFsOptionsBuilder(Configuration config) {
     return GoogleCloudStorageFileSystemOptions.builder()
         .setCloudStorageOptions(getGcsOptionsBuilder(config).build())
