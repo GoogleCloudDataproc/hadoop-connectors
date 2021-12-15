712fb6718167a25a4e30cdd619f22664af4c5128
Prashanth Sagar <mprashanthsagar@gmail.com>
2021-12-15 22:05:11 +0530

    Update GoogleHadoopFileSystemConfigurationTest.java

--- gcs/src/test/java/com/google/cloud/hadoop/fs/gcs/GoogleHadoopFileSystemConfigurationTest.java
+++ gcs/src/test/java/com/google/cloud/hadoop/fs/gcs/GoogleHadoopFileSystemConfigurationTest.java
@@ -85,7 +85,7 @@ public class GoogleHadoopFileSystemConfigurationTest {
           put("fs.gs.grpc.read.zerocopy.enable", true);
           put("fs.gs.grpc.directpath.enable", true);
           put("fs.gs.grpc.server.address", "storage.googleapis.com");
-          put("fs.gs.grpc.trafficdirector.enable", true);
+          put("fs.gs.grpc.trafficdirector.enable", false);
           put("fs.gs.grpc.write.buffered.requests", 20L);
           put("fs.gs.grpc.write.timeout.ms", 10 * 60 * 1000L);
           put("fs.gs.http.connect-timeout", 20_000);
