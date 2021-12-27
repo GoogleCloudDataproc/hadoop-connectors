ef21d42f80af455ad094c64e743f5d73b26bdad8
Igor Dvorzhak <idmedb@gmail.com>
2021-12-27 11:33:47 -0800

    Update all dependencies to latest versions

--- bigquery/src/test/java/com/google/cloud/hadoop/io/bigquery/DirectBigQueryRecordReaderTest.java
+++ bigquery/src/test/java/com/google/cloud/hadoop/io/bigquery/DirectBigQueryRecordReaderTest.java
@@ -14,8 +14,8 @@
 package com.google.cloud.hadoop.io.bigquery;
 
 import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
