9e9c91c3e906846201967a4238401832917a44f0
Quentin Castel <quentin.castel@dataiku.com>
2022-01-10 07:10:58 +0100

    remove token endpoint in favor of token server url

--- util-hadoop/src/test/java/com/google/cloud/hadoop/util/HadoopCredentialConfigurationTest.java
+++ util-hadoop/src/test/java/com/google/cloud/hadoop/util/HadoopCredentialConfigurationTest.java
@@ -33,6 +33,7 @@ import static org.junit.Assert.assertThrows;
 
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.auth.oauth2.TokenResponse;
+import com.google.api.client.googleapis.auth.oauth2.GoogleOAuthConstants;
 import com.google.api.client.testing.http.MockHttpTransport;
 import com.google.cloud.hadoop.util.CredentialFactory.GoogleCredentialWithRetry;
 import com.google.common.collect.ImmutableList;
@@ -66,8 +67,7 @@ public class HadoopCredentialConfigurationTest {
           put(".service.account.auth.keyfile", null);
           put(".auth.service.account.private.key", null);
           put(".auth.service.account.private.key.id", null);
-          put(".token.server.url", "https://oauth2.googleapis.com/token");
-          put(".auth.token.endpoint", null);
+          put(".token.server.url", GoogleOAuthConstants.TOKEN_SERVER_URL);
           put(".auth.refresh.token", null);
           put(".auth.client.secret", null);
           put(".auth.client.id", null);
