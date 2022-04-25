f8660bd9dcf4f6a48dd005149e9dba93113575e7
Prashanth Sagar <prsagar@google.com>
2022-04-25 16:57:35 +0530

    Move comments

--- gcsio/src/main/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageFileSystem.java
+++ gcsio/src/main/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageFileSystem.java
@@ -17,7 +17,7 @@
 package com.google.cloud.hadoop.gcsio;
 
 import com.google.cloud.hadoop.gcsio.GoogleCloudStorage.ListPage;
-import com.google.common.annotations.VisibleForTesting;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URI;
 import java.nio.channels.SeekableByteChannel;
@@ -32,50 +32,242 @@ public interface GoogleCloudStorageFileSystem {
   // URI of the root path.
   URI GCS_ROOT = URI.create(SCHEME + ":/");
 
+  /**
+   * Creates and opens an object for writing. If the object already exists, it is deleted.
+   *
+   * @param path Object full path of the form gs://bucket/object-path.
+   * @return A channel for writing to the given object.
+   */
   WritableByteChannel create(URI path) throws IOException;
 
+  /**
+   * Creates and opens an object for writing.
+   *
+   * @param path Object full path of the form gs://bucket/object-path.
+   * @return A channel for writing to the given object.
+   */
   WritableByteChannel create(URI path, CreateFileOptions createOptions) throws IOException;
 
+  /**
+   * Opens an object for reading.
+   *
+   * @param path Object full path of the form gs://bucket/object-path.
+   * @return A channel for reading from the given object.
+   * @throws FileNotFoundException if the given path does not exist.
+   * @throws IOException if object exists but cannot be opened.
+   */
   SeekableByteChannel open(URI path) throws IOException;
 
+  /**
+   * Opens an object for reading.
+   *
+   * @param path Object full path of the form gs://bucket/object-path.
+   * @param readOptions Fine-grained options for behaviors of retries, buffering, etc.
+   * @return A channel for reading from the given object.
+   * @throws FileNotFoundException if the given path does not exist.
+   * @throws IOException if object exists but cannot be opened.
+   */
   SeekableByteChannel open(URI path, GoogleCloudStorageReadOptions readOptions) throws IOException;
 
+  /**
+   * Opens an object for reading using {@link FileInfo}.
+   *
+   * @param fileInfo contains metadata information about the file
+   * @param readOptions readOptions fine-grained options specifying things like retry settings,
+   *     buffering, etc.
+   * @return Seekable Byte Channel to enable file open
+   * @throws IOException IOException on IO Error
+   */
   SeekableByteChannel open(FileInfo fileInfo, GoogleCloudStorageReadOptions readOptions)
       throws IOException;
 
+  /**
+   * Deletes one or more items indicated by the given path.
+   *
+   * <p>If path points to a directory:
+   *
+   * <ul>
+   *   <li>if recursive is true, all items under that path are recursively deleted followed by
+   *       deletion of the directory.
+   *   <li>else,
+   *       <ul>
+   *         <li>the directory is deleted if it is empty,
+   *         <li>else, an IOException is thrown.
+   *       </ul>
+   * </ul>
+   *
+   * <p>The recursive parameter is ignored for a file.
+   *
+   * @param path Path of the item to delete.
+   * @param recursive If true, all sub-items are also deleted.
+   * @throws FileNotFoundException if the given path does not exist.
+   */
   void delete(URI path, boolean recursive) throws IOException;
 
+  /**
+   * Indicates whether the given item exists.
+   *
+   * @param path Path of the item to check.
+   * @return true if the given item exists, false otherwise.
+   */
   boolean exists(URI path) throws IOException;
 
+  /**
+   * Creates a directory at the specified path. Also creates any parent directories as necessary.
+   * Similar to 'mkdir -p' command.
+   *
+   * @param path Path of the directory to create.
+   */
   void mkdirs(URI path) throws IOException;
 
+  /**
+   * Renames the given item's path.
+   *
+   * <p>The operation is disallowed if any of the following is true:
+   *
+   * <ul>
+   *   <li>src equals GCS_ROOT
+   *   <li>src is a file and dst equals GCS_ROOT
+   *   <li>src does not exist
+   *   <li>dst is a file that already exists
+   *   <li>parent of the destination does not exist
+   * </ul>
+   *
+   * <p>Otherwise, the expected behavior is as follows:
+   *
+   * <ul>
+   *   <li>if src is a directory:
+   *       <ul>
+   *         <li>if dst is an existing file then disallowed
+   *         <li>if dst is a directory then rename the directory
+   *       </ul>
+   *       <p>
+   *   <li>if src is a file:
+   *       <ul>
+   *         <li>if dst is a file then rename the file.
+   *         <li>if dst is a directory then similar to the previous case after appending src
+   *             file-name to dst
+   *       </ul>
+   * </ul>
+   *
+   * <p>Note: This function is very expensive to call for directories that have many sub-items.
+   *
+   * @param src Path of the item to rename.
+   * @param dst New path of the item.
+   * @throws FileNotFoundException if src does not exist.
+   */
   void rename(URI src, URI dst) throws IOException;
 
+  /**
+   * Composes inputs into a single GCS object. This performs a GCS Compose. Objects will be composed
+   * according to the order they appear in the input. The destination object, if already present,
+   * will be overwritten. Sources and destination are assumed to be in the same bucket.
+   *
+   * @param sources the list of URIs to be composed
+   * @param destination the resulting URI with composed sources
+   * @param contentType content-type of the composed object
+   * @throws IOException if the Compose operation was unsuccessful
+   */
   void compose(List<URI> sources, URI destination, String contentType) throws IOException;
 
+  /**
+   * Equivalent to a recursive listing of {@code prefix}, except that {@code prefix} doesn't have to
+   * represent an actual object but can just be a partial prefix string. The 'authority' component
+   * of the {@code prefix} <b>must</b> be the complete authority, however; we can only list prefixes
+   * of <b>objects</b>, not buckets.
+   *
+   * @param prefix the prefix to use to list all matching objects.
+   */
   List<FileInfo> listFileInfoForPrefix(URI prefix) throws IOException;
 
+  /**
+   * Equivalent to a recursive listing of {@code prefix}, except that {@code prefix} doesn't have to
+   * represent an actual object but can just be a partial prefix string. The 'authority' component
+   * of the {@code prefix} <b>must</b> be the complete authority, however; we can only list prefixes
+   * of <b>objects</b>, not buckets.
+   *
+   * @param prefix the prefix to use to list all matching objects.
+   */
   List<FileInfo> listFileInfoForPrefix(URI prefix, ListFileOptions listOptions) throws IOException;
 
+  /**
+   * Equivalent to {@link #listFileInfoForPrefix} but returns {@link FileInfo}s listed by single
+   * request (1 page).
+   *
+   * @param prefix the prefix to use to list all matching objects.
+   * @param pageToken the page token to list
+   */
   ListPage<FileInfo> listFileInfoForPrefixPage(URI prefix, String pageToken) throws IOException;
 
+  /**
+   * Equivalent to {@link #listFileInfoForPrefix} but returns {@link FileInfo}s listed by single
+   * request (1 page).
+   *
+   * @param prefix the prefix to use to list all matching objects.
+   * @param pageToken the page token to list
+   */
   ListPage<FileInfo> listFileInfoForPrefixPage(
       URI prefix, ListFileOptions listOptions, String pageToken) throws IOException;
 
+  /**
+   * If the given path points to a directory then the information about its children is returned,
+   * otherwise information about the given file is returned.
+   *
+   * @param path Given path.
+   * @return Information about a file or children of a directory.
+   * @throws FileNotFoundException if the given path does not exist.
+   */
   List<FileInfo> listFileInfo(URI path) throws IOException;
 
+  /**
+   * If the given path points to a directory then the information about its children is returned,
+   * otherwise information about the given file is returned.
+   *
+   * @param path Given path.
+   * @return Information about a file or children of a directory.
+   * @throws FileNotFoundException if the given path does not exist.
+   */
   List<FileInfo> listFileInfo(URI path, ListFileOptions listOptions) throws IOException;
 
+  /**
+   * Gets information about the given path item.
+   *
+   * @param path The path we want information about.
+   * @return Information about the given path item.
+   */
   FileInfo getFileInfo(URI path) throws IOException;
 
+  /**
+   * Gets information about each path in the given list; more efficient than calling getFileInfo()
+   * on each path individually in a loop.
+   *
+   * @param paths List of paths.
+   * @return Information about each path in the given list.
+   */
   List<FileInfo> getFileInfos(List<URI> paths) throws IOException;
 
+  /** Releases resources used by this instance. */
   void close();
 
-  @VisibleForTesting
+  /**
+   * Creates a directory at the specified path.
+   *
+   * <p>There are two conventions for using objects as directories in GCS. 1. An object of zero size
+   * with name ending in / 2. An object of zero size with name ending in _$folder$
+   *
+   * <p>#1 is the recommended convention by the GCS team. We use it when creating a directory.
+   *
+   * <p>However, some old tools still use #2. We will decide based on customer use cases whether to
+   * support #2 as well. For now, we only support #1.
+   *
+   * <p>Note that a bucket is always considered a directory. Doesn't create parent directories;
+   * normal use cases should only call mkdirs().
+   */
   void mkdir(URI path) throws IOException;
 
+  /** Retrieve our internal gcs. */
   GoogleCloudStorage getGcs();
 
-  public GoogleCloudStorageFileSystemOptions getOptions();
+  /** Retrieve the options that were used to create this GoogleCloudStorageFileSystem. */
+  GoogleCloudStorageFileSystemOptions getOptions();
 }
--- gcsio/src/main/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageFileSystemImpl.java
+++ gcsio/src/main/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageFileSystemImpl.java
@@ -204,7 +204,6 @@ public class GoogleCloudStorageFileSystemImpl implements GoogleCloudStorageFileS
     return service;
   }
 
-  /** Retrieve the options that were used to create this GoogleCloudStorageFileSystem. */
   public GoogleCloudStorageFileSystemOptions getOptions() {
     return options;
   }
@@ -218,24 +217,12 @@ public class GoogleCloudStorageFileSystemImpl implements GoogleCloudStorageFileS
         .build();
   }
 
-  /**
-   * Creates and opens an object for writing. If the object already exists, it is deleted.
-   *
-   * @param path Object full path of the form gs://bucket/object-path.
-   * @return A channel for writing to the given object.
-   */
   @Override
   public WritableByteChannel create(URI path) throws IOException {
     logger.atFiner().log("create(path: %s)", path);
     return create(path, CreateFileOptions.DEFAULT_OVERWRITE);
   }
 
-  /**
-   * Creates and opens an object for writing.
-   *
-   * @param path Object full path of the form gs://bucket/object-path.
-   * @return A channel for writing to the given object.
-   */
   @Override
   public WritableByteChannel create(URI path, CreateFileOptions createOptions) throws IOException {
     logger.atFiner().log("create(path: %s, createOptions: %s)", path, createOptions);
@@ -285,28 +272,11 @@ public class GoogleCloudStorageFileSystemImpl implements GoogleCloudStorageFileS
     return gcs.create(resourceId, objectOptionsFromFileOptions(createOptions));
   }
 
-  /**
-   * Opens an object for reading.
-   *
-   * @param path Object full path of the form gs://bucket/object-path.
-   * @return A channel for reading from the given object.
-   * @throws FileNotFoundException if the given path does not exist.
-   * @throws IOException if object exists but cannot be opened.
-   */
   @Override
   public SeekableByteChannel open(URI path) throws IOException {
     return open(path, GoogleCloudStorageReadOptions.DEFAULT);
   }
 
-  /**
-   * Opens an object for reading.
-   *
-   * @param path Object full path of the form gs://bucket/object-path.
-   * @param readOptions Fine-grained options for behaviors of retries, buffering, etc.
-   * @return A channel for reading from the given object.
-   * @throws FileNotFoundException if the given path does not exist.
-   * @throws IOException if object exists but cannot be opened.
-   */
   @Override
   public SeekableByteChannel open(URI path, GoogleCloudStorageReadOptions readOptions)
       throws IOException {
@@ -319,15 +289,6 @@ public class GoogleCloudStorageFileSystemImpl implements GoogleCloudStorageFileS
     return gcs.open(resourceId, readOptions);
   }
 
-  /**
-   * Opens an object for reading using {@link FileInfo}.
-   *
-   * @param fileInfo contains metadata information about the file
-   * @param readOptions readOptions fine-grained options specifying things like retry settings,
-   *     buffering, etc.
-   * @return Seekable Byte Channel to enable file open
-   * @throws IOException IOException on IO Error
-   */
   @Override
   public SeekableByteChannel open(FileInfo fileInfo, GoogleCloudStorageReadOptions readOptions)
       throws IOException {
@@ -339,27 +300,6 @@ public class GoogleCloudStorageFileSystemImpl implements GoogleCloudStorageFileS
     return gcs.open(fileInfo.getItemInfo(), readOptions);
   }
 
-  /**
-   * Deletes one or more items indicated by the given path.
-   *
-   * <p>If path points to a directory:
-   *
-   * <ul>
-   *   <li>if recursive is true, all items under that path are recursively deleted followed by
-   *       deletion of the directory.
-   *   <li>else,
-   *       <ul>
-   *         <li>the directory is deleted if it is empty,
-   *         <li>else, an IOException is thrown.
-   *       </ul>
-   * </ul>
-   *
-   * <p>The recursive parameter is ignored for a file.
-   *
-   * @param path Path of the item to delete.
-   * @param recursive If true, all sub-items are also deleted.
-   * @throws FileNotFoundException if the given path does not exist.
-   */
   @Override
   public void delete(URI path, boolean recursive) throws IOException {
     checkNotNull(path, "path should not be null");
@@ -447,24 +387,12 @@ public class GoogleCloudStorageFileSystemImpl implements GoogleCloudStorageFileS
     }
   }
 
-  /**
-   * Indicates whether the given item exists.
-   *
-   * @param path Path of the item to check.
-   * @return true if the given item exists, false otherwise.
-   */
   @Override
   public boolean exists(URI path) throws IOException {
     logger.atFiner().log("exists(path: %s)", path);
     return getFileInfo(path).exists();
   }
 
-  /**
-   * Creates a directory at the specified path. Also creates any parent directories as necessary.
-   * Similar to 'mkdir -p' command.
-   *
-   * @param path Path of the directory to create.
-   */
   @Override
   public void mkdirs(URI path) throws IOException {
     logger.atFiner().log("mkdirs(path: %s)", path);
@@ -510,42 +438,6 @@ public class GoogleCloudStorageFileSystemImpl implements GoogleCloudStorageFileS
     }
   }
 
-  /**
-   * Renames the given item's path.
-   *
-   * <p>The operation is disallowed if any of the following is true:
-   *
-   * <ul>
-   *   <li>src equals GCS_ROOT
-   *   <li>src is a file and dst equals GCS_ROOT
-   *   <li>src does not exist
-   *   <li>dst is a file that already exists
-   *   <li>parent of the destination does not exist
-   * </ul>
-   *
-   * <p>Otherwise, the expected behavior is as follows:
-   *
-   * <ul>
-   *   <li>if src is a directory:
-   *       <ul>
-   *         <li>if dst is an existing file then disallowed
-   *         <li>if dst is a directory then rename the directory
-   *       </ul>
-   *       <p>
-   *   <li>if src is a file:
-   *       <ul>
-   *         <li>if dst is a file then rename the file.
-   *         <li>if dst is a directory then similar to the previous case after appending src
-   *             file-name to dst
-   *       </ul>
-   * </ul>
-   *
-   * <p>Note: This function is very expensive to call for directories that have many sub-items.
-   *
-   * @param src Path of the item to rename.
-   * @param dst New path of the item.
-   * @throws FileNotFoundException if src does not exist.
-   */
   @Override
   public void rename(URI src, URI dst) throws IOException {
     logger.atFiner().log("rename(src: %s, dst: %s)", src, dst);
@@ -696,16 +588,6 @@ public class GoogleCloudStorageFileSystemImpl implements GoogleCloudStorageFileS
     return dst;
   }
 
-  /**
-   * Composes inputs into a single GCS object. This performs a GCS Compose. Objects will be composed
-   * according to the order they appear in the input. The destination object, if already present,
-   * will be overwritten. Sources and destination are assumed to be in the same bucket.
-   *
-   * @param sources the list of URIs to be composed
-   * @param destination the resulting URI with composed sources
-   * @param contentType content-type of the composed object
-   * @throws IOException if the Compose operation was unsuccessful
-   */
   @Override
   public void compose(List<URI> sources, URI destination, String contentType) throws IOException {
     StorageResourceId destResource = StorageResourceId.fromStringPath(destination.toString());
@@ -853,27 +735,11 @@ public class GoogleCloudStorageFileSystemImpl implements GoogleCloudStorageFileS
     }
   }
 
-  /**
-   * Equivalent to a recursive listing of {@code prefix}, except that {@code prefix} doesn't have to
-   * represent an actual object but can just be a partial prefix string. The 'authority' component
-   * of the {@code prefix} <b>must</b> be the complete authority, however; we can only list prefixes
-   * of <b>objects</b>, not buckets.
-   *
-   * @param prefix the prefix to use to list all matching objects.
-   */
   @Override
   public List<FileInfo> listFileInfoForPrefix(URI prefix) throws IOException {
     return listFileInfoForPrefix(prefix, ListFileOptions.DEFAULT);
   }
 
-  /**
-   * Equivalent to a recursive listing of {@code prefix}, except that {@code prefix} doesn't have to
-   * represent an actual object but can just be a partial prefix string. The 'authority' component
-   * of the {@code prefix} <b>must</b> be the complete authority, however; we can only list prefixes
-   * of <b>objects</b>, not buckets.
-   *
-   * @param prefix the prefix to use to list all matching objects.
-   */
   @Override
   public List<FileInfo> listFileInfoForPrefix(URI prefix, ListFileOptions listOptions)
       throws IOException {
@@ -889,26 +755,12 @@ public class GoogleCloudStorageFileSystemImpl implements GoogleCloudStorageFileS
     return fileInfos;
   }
 
-  /**
-   * Equivalent to {@link #listFileInfoForPrefix} but returns {@link FileInfo}s listed by single
-   * request (1 page).
-   *
-   * @param prefix the prefix to use to list all matching objects.
-   * @param pageToken the page token to list
-   */
   @Override
   public ListPage<FileInfo> listFileInfoForPrefixPage(URI prefix, String pageToken)
       throws IOException {
     return listFileInfoForPrefixPage(prefix, ListFileOptions.DEFAULT, pageToken);
   }
 
-  /**
-   * Equivalent to {@link #listFileInfoForPrefix} but returns {@link FileInfo}s listed by single
-   * request (1 page).
-   *
-   * @param prefix the prefix to use to list all matching objects.
-   * @param pageToken the page token to list
-   */
   @Override
   public ListPage<FileInfo> listFileInfoForPrefixPage(
       URI prefix, ListFileOptions listOptions, String pageToken) throws IOException {
@@ -935,27 +787,11 @@ public class GoogleCloudStorageFileSystemImpl implements GoogleCloudStorageFileS
     return prefixId;
   }
 
-  /**
-   * If the given path points to a directory then the information about its children is returned,
-   * otherwise information about the given file is returned.
-   *
-   * @param path Given path.
-   * @return Information about a file or children of a directory.
-   * @throws FileNotFoundException if the given path does not exist.
-   */
   @Override
   public List<FileInfo> listFileInfo(URI path) throws IOException {
     return listFileInfo(path, ListFileOptions.DEFAULT);
   }
 
-  /**
-   * If the given path points to a directory then the information about its children is returned,
-   * otherwise information about the given file is returned.
-   *
-   * @param path Given path.
-   * @return Information about a file or children of a directory.
-   * @throws FileNotFoundException if the given path does not exist.
-   */
   @Override
   public List<FileInfo> listFileInfo(URI path, ListFileOptions listOptions) throws IOException {
     checkNotNull(path, "path can not be null");
@@ -1005,12 +841,6 @@ public class GoogleCloudStorageFileSystemImpl implements GoogleCloudStorageFileS
     return fileInfos;
   }
 
-  /**
-   * Gets information about the given path item.
-   *
-   * @param path The path we want information about.
-   * @return Information about the given path item.
-   */
   @Override
   public FileInfo getFileInfo(URI path) throws IOException {
     checkArgument(path != null, "path must not be null");
@@ -1073,13 +903,6 @@ public class GoogleCloudStorageFileSystemImpl implements GoogleCloudStorageFileS
         : GoogleCloudStorageItemInfo.createNotFound(resourceId);
   }
 
-  /**
-   * Gets information about each path in the given list; more efficient than calling getFileInfo()
-   * on each path individually in a loop.
-   *
-   * @param paths List of paths.
-   * @return Information about each path in the given list.
-   */
   @Override
   public List<FileInfo> getFileInfos(List<URI> paths) throws IOException {
     checkArgument(paths != null, "paths must not be null");
@@ -1111,7 +934,6 @@ public class GoogleCloudStorageFileSystemImpl implements GoogleCloudStorageFileS
     }
   }
 
-  /** Releases resources used by this instance. */
   @Override
   public void close() {
     if (gcs == null) {
@@ -1129,20 +951,6 @@ public class GoogleCloudStorageFileSystemImpl implements GoogleCloudStorageFileS
     }
   }
 
-  /**
-   * Creates a directory at the specified path.
-   *
-   * <p>There are two conventions for using objects as directories in GCS. 1. An object of zero size
-   * with name ending in / 2. An object of zero size with name ending in _$folder$
-   *
-   * <p>#1 is the recommended convention by the GCS team. We use it when creating a directory.
-   *
-   * <p>However, some old tools still use #2. We will decide based on customer use cases whether to
-   * support #2 as well. For now, we only support #1.
-   *
-   * <p>Note that a bucket is always considered a directory. Doesn't create parent directories;
-   * normal use cases should only call mkdirs().
-   */
   @Override
   @VisibleForTesting
   public void mkdir(URI path) throws IOException {
@@ -1245,7 +1053,6 @@ public class GoogleCloudStorageFileSystemImpl implements GoogleCloudStorageFileS
     return listObjectOptions.toBuilder().setFields(listFileOptions.getFields()).build();
   }
 
-  /** Retrieve our internal gcs. */
   @Override
   public GoogleCloudStorage getGcs() {
     return gcs;
