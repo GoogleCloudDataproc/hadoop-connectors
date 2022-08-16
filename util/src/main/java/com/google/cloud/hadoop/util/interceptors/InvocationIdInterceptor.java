7ba5dd11bf3ef562cd1c8775f9a11ac3164e3387
Ravi Dutt Singh <singhravidutt@gmail.com>
2022-08-16 15:33:32 +0530

    addressed review comments

--- util/src/main/java/com/google/cloud/hadoop/util/interceptors/InvocationIdInterceptor.java
+++ util/src/main/java/com/google/cloud/hadoop/util/interceptors/InvocationIdInterceptor.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 Google LLC. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.cloud.hadoop.util.interceptors;
 
 import com.google.api.client.http.HttpExecuteInterceptor;
@@ -6,6 +22,10 @@ import com.google.api.client.http.HttpRequest;
 import java.io.IOException;
 import java.util.UUID;
 
+/**
+ * HTTP request interceptor to attach unique identifier i.e. invocationId to each new request and
+ * make sure to pass in the same identifier if request is retried.
+ */
 public final class InvocationIdInterceptor implements HttpExecuteInterceptor {
 
   public static final String GCCL_INVOCATION_ID_PREFIX = "gccl-invocation-id/";
@@ -30,10 +50,11 @@ public final class InvocationIdInterceptor implements HttpExecuteInterceptor {
       // TODO: add support for attempt_count
       return;
     }
-    UUID invocationId = UUID.randomUUID();
+
     final String signatureKey = "Signature="; // For V2 and V4 signedURLs
     final String builtURL = request.getUrl().build();
-    if (invocationId != null && !builtURL.contains(signatureKey)) {
+    if (!builtURL.contains(signatureKey)) {
+      UUID invocationId = UUID.randomUUID();
       String invocationEntry = GCCL_INVOCATION_ID_PREFIX + invocationId;
       final String newValue;
       if (existing != null && !existing.isEmpty()) {
@@ -41,11 +62,13 @@ public final class InvocationIdInterceptor implements HttpExecuteInterceptor {
       } else {
         newValue = invocationEntry;
       }
-      headers.set("x-goog-api-client", newValue);
+      headers.set(GOOG_API_CLIENT, newValue);
     }
   }
 
   private boolean isInvocationIdPresent(String apiClientHeader) {
-    return apiClientHeader != null ? apiClientHeader.contains(GCCL_INVOCATION_ID_PREFIX) : false;
+    return apiClientHeader != null && apiClientHeader.contains(GCCL_INVOCATION_ID_PREFIX)
+        ? true
+        : false;
   }
 }
