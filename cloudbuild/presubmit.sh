cb0ddddcfa3f765993f769c564fbc3fd993aee16
Ravi Dutt Singh <singhravidutt@gmail.com>
2023-02-08 17:43:22 +0530

    adding mapping entry for GCE metadata service

--- cloudbuild/presubmit.sh
+++ cloudbuild/presubmit.sh
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+echo 169.254.169.254 metadata.google.internal > /etc/hosts
+
 set -euxo pipefail
 
 readonly TEST_TYPE="${1:-unittest}"
