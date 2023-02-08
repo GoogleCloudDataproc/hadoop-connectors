b65326748a7af6c0a201fcebd0f17e5f4154fd59
Ravi Dutt Singh <singhravidutt@gmail.com>
2023-02-09 01:52:19 +0530

    cleanup

--- cloudbuild/directpathChecker.sh
+++ cloudbuild/directpathChecker.sh
@@ -1,7 +1,3 @@
-echo "169.254.169.254 metadata.google.internal" >> /etc/hosts
-
-cat /etc/hosts
-
 
 curl -L \
 https://github.com/GoogleCloudPlatform/grpc-gcp-tools/releases/latest/download/dp_check -o dp_check
