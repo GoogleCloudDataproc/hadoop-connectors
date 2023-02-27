b769cc3a686d233d1c4da084d569c357a10d7a56
Ravi Singh <singhravidutt@gmail.com>
2023-02-27 18:40:24 +0530

    encryption/decryption support in java-storage (#968) (#970)

--- gcsio/src/main/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageClientReadChannel.java
+++ gcsio/src/main/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageClientReadChannel.java
@@ -51,6 +51,7 @@ class GoogleCloudStorageClientReadChannel implements SeekableByteChannel {
 
   private final StorageResourceId resourceId;
   private final GoogleCloudStorageReadOptions readOptions;
+  private final GoogleCloudStorageOptions storageOptions;
   private final Storage storage;
   // The size of this object generation, in bytes.
   private final long objectSize;
@@ -65,7 +66,8 @@ class GoogleCloudStorageClientReadChannel implements SeekableByteChannel {
   public GoogleCloudStorageClientReadChannel(
       Storage storage,
       GoogleCloudStorageItemInfo itemInfo,
-      GoogleCloudStorageReadOptions readOptions)
+      GoogleCloudStorageReadOptions readOptions,
+      GoogleCloudStorageOptions storageOptions)
       throws IOException {
     validate(itemInfo);
     this.storage = storage;
@@ -73,6 +75,7 @@ class GoogleCloudStorageClientReadChannel implements SeekableByteChannel {
         new StorageResourceId(
             itemInfo.getBucketName(), itemInfo.getObjectName(), itemInfo.getContentGeneration());
     this.readOptions = readOptions;
+    this.storageOptions = storageOptions;
     this.objectSize = itemInfo.getSize();
     this.contentReadChannel = new ContentReadChannel(readOptions, resourceId);
   }
@@ -490,11 +493,15 @@ class GoogleCloudStorageClientReadChannel implements SeekableByteChannel {
     }
 
     private BlobSourceOption[] generateReadOptions(BlobId blobId) {
-      List<BlobSourceOption> readOptions = new ArrayList<>();
+      List<BlobSourceOption> blobReadOptions = new ArrayList<>();
       if (blobId.getGeneration() != null) {
-        readOptions.add(BlobSourceOption.generationMatch(blobId.getGeneration()));
+        blobReadOptions.add(BlobSourceOption.generationMatch(blobId.getGeneration()));
       }
-      return readOptions.toArray(new BlobSourceOption[readOptions.size()]);
+      if (storageOptions.getEncryptionKey() != null) {
+        blobReadOptions.add(
+            BlobSourceOption.decryptionKey(storageOptions.getEncryptionKey().value()));
+      }
+      return blobReadOptions.toArray(new BlobSourceOption[blobReadOptions.size()]);
     }
 
     private boolean isFooterRead() {
