76b29b9a36f84a9fdc484541014f3ae57b32c464
priyanshu-beep <priyanshujha@google.com>
2023-06-14 09:23:06 +0000

    add high performance read vector API

--- gcs/src/main/java/com/google/cloud/hadoop/fs/gcs/GoogleHadoopFSInputStream.java
+++ gcs/src/main/java/com/google/cloud/hadoop/fs/gcs/GoogleHadoopFSInputStream.java
@@ -17,6 +17,7 @@
 package com.google.cloud.hadoop.fs.gcs;
 
 import com.google.cloud.hadoop.gcsio.GoogleCloudStorageReadOptions;
+import com.google.cloud.hadoop.gcsio.GoogleCloudStorageReadOptions.Fadvise;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Stopwatch;
@@ -24,21 +25,36 @@ import com.google.common.flogger.GoogleLogger;
 import com.google.common.flogger.LazyArgs;
 import com.google.gson.Gson;
 import java.io.IOException;
+import java.io.InterruptedIOException;
 import java.net.URI;
 import java.nio.ByteBuffer;
 import java.nio.channels.ClosedChannelException;
 import java.nio.channels.SeekableByteChannel;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.function.IntFunction;
 import org.apache.hadoop.fs.FSInputStream;
+import org.apache.hadoop.fs.FileRange;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.VectoredReadUtils;
+import org.apache.hadoop.fs.impl.CombinedFileRange;
 
 /** A seekable and positionable FSInputStream that provides read access to a file. */
 class GoogleHadoopFSInputStream extends FSInputStream {
 
   private static final GoogleLogger logger = GoogleLogger.forEnclosingClass();
   @VisibleForTesting static final String READ_METHOD = "gcsFSRead";
+  @VisibleForTesting static final String READ_VECTORED_METHOD = "readVectored";
   @VisibleForTesting static final String POSITIONAL_READ_METHOD = "gcsFSReadPositional";
   @VisibleForTesting static final String SEEK_METHOD = "gcsFSSeek";
   @VisibleForTesting static final String CLOSE_METHOD = "gcsFSClose";
@@ -56,6 +72,12 @@ class GoogleHadoopFSInputStream extends FSInputStream {
 
   // All store IO access goes through this.
   private final SeekableByteChannel channel;
+  private final GoogleCloudStorageReadOptions readVectoredOptions;
+  private final GoogleHadoopFileSystemBase ghfs;
+  private final int maxReadVectoredParallelism = 8;
+  private final int minimumRangeSize = 2 * 1024 * 1024;
+  private final int maximumRangeSize = 128 * 1024 * 1024;
+  private final ThreadPoolExecutor unboundedThreadPool;
 
   // Path of the file to read.
   private URI gcsPath;
@@ -91,6 +113,238 @@ class GoogleHadoopFSInputStream extends FSInputStream {
     this.totalBytesRead = 0;
     this.isTraceLoggingEnabled = readOptions.isTraceLogEnabled();
     this.channel = ghfs.getGcsFs().open(gcsPath, readOptions);
+    this.readVectoredOptions = getReadVectoredOptions(readOptions);
+    this.ghfs = ghfs;
+    this.unboundedThreadPool =
+        new ThreadPoolExecutor(
+            maxReadVectoredParallelism,
+            10000000,
+            3L,
+            TimeUnit.SECONDS,
+            new LinkedBlockingQueue<>());
+  }
+
+  /**
+   * Returns modified GoogleCloudStorageReadOptions with vectored read options set.
+   *
+   * @param readOptions The original read options.
+   * @return The modified read options with vectored read options set.
+   */
+  private GoogleCloudStorageReadOptions getReadVectoredOptions(
+      GoogleCloudStorageReadOptions readOptions) {
+    GoogleCloudStorageReadOptions.Builder builder = readOptions.toBuilder();
+    builder.setFastFailOnNotFound(false);
+    builder.setFadvise(Fadvise.SEQUENTIAL);
+    return builder.build();
+  }
+
+  /**
+   * Read data from GCS for this range and populate the buffer.
+   *
+   * @param range range of data to read.
+   * @param buffer buffer to fill.
+   */
+  private void readSingleRange(FileRange range, ByteBuffer buffer) {
+    try {
+      VectoredReadUtils.validateRangeRequest(range);
+      try (SeekableByteChannel channel = ghfs.getGcsFs().open(gcsPath, readVectoredOptions)) {
+        channel.position(range.getOffset());
+        int numRead = channel.read(ByteBuffer.wrap(buffer.array(), 0, range.getLength()));
+        range.getData().complete(buffer);
+      }
+    } catch (Exception ex) {
+      logger.atInfo().withCause(ex).log("Exception while reading a range %s", range.toString());
+      range.getData().completeExceptionally(ex);
+    }
+  }
+
+  /**
+   * Calculates the optimal range size based on the given list of file ranges and the maximum
+   * parallelism.
+   *
+   * @param ranges The list of file ranges to determine the optimal range size for.
+   * @return The optimal range size.
+   */
+  private int getOptimalRangeSize(List<? extends FileRange> ranges) {
+    int l = minimumRangeSize;
+    int r = maximumRangeSize + 1;
+
+    while (l < r) {
+      int mid = (l + r) / 2;
+      int count = 0;
+      for (FileRange range : ranges) {
+        count += (range.getLength() + mid - 1) / mid;
+      }
+
+      if (count > maxReadVectoredParallelism) {
+        l = mid + 1;
+      } else {
+        r = mid;
+      }
+    }
+    return r;
+  }
+
+  /**
+   * Splits and reads the ranges in parallel, combining the split ranges into the original ranges.
+   *
+   * @param ranges the byte ranges to read.
+   * @param allocate the function to allocate ByteBuffer.
+   * @throws IOException if an I/O error occurs during reading.
+   */
+  public void splitAndReadRanges(List<? extends FileRange> ranges, IntFunction<ByteBuffer> allocate)
+      throws IOException {
+    int optimalRangeSize = getOptimalRangeSize(ranges);
+
+    List<FileRange> list = new ArrayList<>();
+    for (FileRange range : ranges) {
+      long offset = range.getOffset();
+      int length = range.getLength();
+      while (length > 0) {
+        int curLength = Math.min(length, optimalRangeSize);
+        CompletableFuture<ByteBuffer> result = new CompletableFuture<>();
+        FileRange fileRange = FileRange.createFileRange(offset, curLength);
+        fileRange.setData(result);
+        list.add(fileRange);
+        offset += curLength;
+        length -= curLength;
+      }
+    }
+
+    for (FileRange range : list) {
+      ByteBuffer buffer = allocate.apply(range.getLength());
+      unboundedThreadPool.submit(() -> readSingleRange(range, buffer));
+    }
+
+    int k = 0;
+    for (FileRange range : ranges) {
+      int length = range.getLength();
+      ByteBuffer buffer = allocate.apply(range.getLength());
+      while (length > 0) {
+        int curLength = Math.min(length, optimalRangeSize);
+        ByteBuffer buff = awaitFuture(list.get(k++).getData());
+        buffer.put(buff);
+        length -= curLength;
+        statistics.incrementBytesRead(curLength);
+        statistics.incrementReadOps(1);
+        totalBytesRead += curLength;
+      }
+      buffer.flip();
+      range.getData().complete(buffer);
+    }
+  }
+
+  /**
+   * {@inheritDoc} Vectored read implementation for S3AInputStream.
+   *
+   * @param ranges the byte ranges to read.
+   * @param allocate the function to allocate ByteBuffer.
+   * @throws IOException IOE if any.
+   */
+  @Override
+  public void readVectored(List<? extends FileRange> ranges, IntFunction<ByteBuffer> allocate)
+      throws IOException {
+
+    Stopwatch stopwatch = Stopwatch.createStarted();
+    long bytesRead = 0;
+
+    for (FileRange range : ranges) {
+      VectoredReadUtils.validateRangeRequest(range);
+      CompletableFuture<ByteBuffer> result = new CompletableFuture<>();
+      range.setData(result);
+      bytesRead += range.getLength();
+    }
+
+    if (VectoredReadUtils.isOrderedDisjoint(ranges, 1, minSeekForVectorReads())) {
+      splitAndReadRanges(ranges, allocate);
+    } else {
+      List<CombinedFileRange> combinedFileRanges =
+          sortAndMergeRanges(ranges, 1, minSeekForVectorReads(), maxReadSizeForVectorReads());
+      for (CombinedFileRange combinedFileRange : combinedFileRanges) {
+        CompletableFuture<ByteBuffer> result = new CompletableFuture<>();
+        combinedFileRange.setData(result);
+      }
+
+      splitAndReadRanges(combinedFileRanges, allocate);
+      for (CombinedFileRange combinedFileRange : combinedFileRanges) {
+        try {
+          ByteBuffer combinedBuffer = awaitFuture(combinedFileRange.getData());
+          for (FileRange child : combinedFileRange.getUnderlying()) {
+            updateOriginalRange(child, combinedBuffer, combinedFileRange);
+          }
+        } catch (Exception ex) {
+          logger.atSevere().withCause(ex).log(
+              "Exception occurred while reading combined range from file");
+          for (FileRange child : combinedFileRange.getUnderlying()) {
+            child.getData().completeExceptionally(ex);
+          }
+        }
+      }
+    }
+    stopwatch.stop();
+    readVectoredTrace(READ_VECTORED_METHOD, stopwatch, bytesRead);
+  }
+
+  /**
+   * Update data in child range from combined range.
+   *
+   * @param child child range.
+   * @param combinedBuffer combined buffer.
+   * @param combinedFileRange combined range.
+   */
+  private void updateOriginalRange(
+      FileRange child, ByteBuffer combinedBuffer, CombinedFileRange combinedFileRange) {
+    ByteBuffer childBuffer =
+        VectoredReadUtils.sliceTo(combinedBuffer, combinedFileRange.getOffset(), child);
+    child.getData().complete(childBuffer);
+  }
+  /**
+   * Sort and merge ranges to optimize the access from the underlying file system.
+   *
+   * @param input list of ranges based on offset.
+   * @param chunkSize round the start and end points to multiples of chunkSize
+   * @param minimumSeek the smallest gap that we should seek over in bytes
+   * @param maxSize the largest org.apache.hadoop.shaded.com.ined file range in bytes
+   * @return the list of sorted CombinedFileRanges that cover the input
+   */
+  private List<CombinedFileRange> sortAndMergeRanges(
+      List<? extends FileRange> input, int chunkSize, int minimumSeek, int maxSize) {
+    // sort the ranges by offset
+    FileRange[] ranges = input.toArray(new FileRange[0]);
+    Arrays.sort(ranges, Comparator.comparingLong(FileRange::getOffset));
+    CombinedFileRange current = null;
+    List<CombinedFileRange> result = new ArrayList<>(ranges.length);
+
+    // now merge together the ones that merge
+    for (FileRange range : ranges) {
+      long start = range.getOffset();
+      long end = start + range.getLength();
+      if (current == null || !current.merge(start, end, range, minimumSeek, maxSize)) {
+        current = new CombinedFileRange(start, end, range);
+        result.add(current);
+      }
+    }
+    return result;
+  }
+
+  /**
+   * Waits for the completion of a Future and returns its result.
+   *
+   * @param future The Future to await completion for.
+   * @param <T> The type of the Future's result.
+   * @return The result of the completed Future.
+   * @throws IOException if an I/O error occurs while waiting for the Future's completion.
+   * @throws InterruptedIOException if the waiting thread is interrupted.
+   */
+  public static <T> T awaitFuture(final Future<T> future)
+      throws IOException, InterruptedIOException {
+    try {
+      return future.get();
+    } catch (InterruptedException e) {
+      throw (InterruptedIOException) new InterruptedIOException(e.toString()).initCause(e);
+    } catch (ExecutionException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   /**
@@ -280,6 +534,17 @@ class GoogleHadoopFSInputStream extends FSInputStream {
       jsonMap.put(GCS_PATH, gcsPath);
       jsonMap.put(DURATION_NS, stopwatch.elapsed(TimeUnit.NANOSECONDS));
       jsonMap.put(POSITION, pos);
+      captureAPITraces(jsonMap);
+    }
+  }
+
+  private void readVectoredTrace(String method, Stopwatch stopwatch, long bytesRead) {
+    if (isTraceLoggingEnabled) {
+      Map<String, Object> jsonMap = new HashMap<>();
+      jsonMap.put(METHOD, method);
+      jsonMap.put(GCS_PATH, gcsPath);
+      jsonMap.put(DURATION_NS, stopwatch.elapsed(TimeUnit.NANOSECONDS));
+      jsonMap.put(BYTES_READ, bytesRead);
       captureAPITraces(jsonMap);
     }
   }
