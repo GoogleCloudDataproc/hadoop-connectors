7d3de2e5d56e6ff87f768790e9f4176b47bec18a
Ravi Dutt Singh <singhravidutt@gmail.com>
2023-09-14 13:55:55 +0530

    cleanup

--- gcsio/src/test/java/com/google/cloud/hadoop/gcsio/integration/GoogleCloudStorageImplTest.java
+++ gcsio/src/test/java/com/google/cloud/hadoop/gcsio/integration/GoogleCloudStorageImplTest.java
@@ -87,7 +87,7 @@ public class GoogleCloudStorageImplTest {
 
   @Parameters
   public static Iterable<Boolean> getTesStorageClientImplParameter() {
-    return List.of(true);
+    return List.of(false, true);
   }
 
   @Rule
