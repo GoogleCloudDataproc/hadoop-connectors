e5fcac8ae22fe4761c4ca02d8b9924205a4edf72
Ravi Singh <singhravidutt@gmail.com>
2023-10-27 13:14:48 +0530

    parallel composite upload support (#1067)

--- gcs/src/test/java/com/google/cloud/hadoop/fs/gcs/GoogleHadoopFileSystemConfigurationTest.java
+++ gcs/src/test/java/com/google/cloud/hadoop/fs/gcs/GoogleHadoopFileSystemConfigurationTest.java
@@ -32,6 +32,7 @@ import com.google.cloud.hadoop.gcsio.GoogleCloudStorageOptions;
 import com.google.cloud.hadoop.gcsio.GoogleCloudStorageOptions.MetricsSink;
 import com.google.cloud.hadoop.gcsio.GoogleCloudStorageReadOptions.Fadvise;
 import com.google.cloud.hadoop.gcsio.PerformanceCachingGoogleCloudStorageOptions;
+import com.google.cloud.hadoop.util.AsyncWriteChannelOptions.PartFileCleanupType;
 import com.google.cloud.hadoop.util.AsyncWriteChannelOptions.PipeType;
 import com.google.cloud.hadoop.util.AsyncWriteChannelOptions.UploadType;
 import com.google.cloud.hadoop.util.RequesterPaysOptions.RequesterPaysMode;
@@ -118,8 +119,14 @@ public class GoogleHadoopFileSystemConfigurationTest {
           put("fs.gs.tracelog.enable", false);
           put("fs.gs.operation.tracelog.enable", false);
           put("fs.gs.working.dir", "/");
-          put("fs.gs.client.upload.type", UploadType.DEFAULT);
+          put("fs.gs.client.upload.type", UploadType.CHUNK_UPLOAD);
           put("fs.gs.write.temporary.dirs", ImmutableSet.of());
+          put("fs.gs.write.parallel.composite.upload.buffer.count", 1);
+          put("fs.gs.write.parallel.composite.upload.buffer.capacity", 32 * 1024 * 1024L);
+          put(
+              "fs.gs.write.parallel.composite.upload.part.file.cleanup.type",
+              PartFileCleanupType.ALWAYS);
+          put("fs.gs.write.parallel.composite.upload.part.file.name.prefix", "");
         }
       };
 
