2e3a4d15f1cff0b5c8e16cbf608631f135446197
Ravi Dutt Singh <rdsingh@google.com>
2024-05-28 14:17:05 +0530

    fixed test case

--- gcs/src/test/java/com/google/cloud/hadoop/fs/gcs/HadoopFileSystemTestBase.java
+++ gcs/src/test/java/com/google/cloud/hadoop/fs/gcs/HadoopFileSystemTestBase.java
@@ -995,6 +995,9 @@ public abstract class HadoopFileSystemTestBase extends GoogleCloudStorageFileSys
   public void testGetFileInfos() {}
 
   @Override
+  public void testGetFileInfoObject() {}
+
+  @Override
   public void testFileCreationSetsAttributes() {}
 
   @Override
