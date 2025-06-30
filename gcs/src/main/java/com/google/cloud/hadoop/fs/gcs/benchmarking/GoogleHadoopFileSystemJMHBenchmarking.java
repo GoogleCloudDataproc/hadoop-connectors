0e9835bd16cd57cac3102be973f3af50bc5276dc
Yash Jain <yashjainn@google.com>
2025-06-30 17:17:20 +0000

    Minor Naming Changes

--- gcs/src/main/java/com/google/cloud/hadoop/fs/gcs/benchmarking/GoogleHadoopFileSystemJMHBenchmarking.java
+++ gcs/src/main/java/com/google/cloud/hadoop/fs/gcs/benchmarking/GoogleHadoopFileSystemJMHBenchmarking.java
@@ -30,7 +30,7 @@ public class GoogleHadoopFileSystemJMHBenchmarking extends GoogleHadoopFileSyste
    * @param action The lambda expression containing the benchmark logic to execute.
    * @throws IOException if the benchmark fails.
    */
-  private void runBenchmarkAndLog(String operationName, BenchmarkAction action) throws IOException {
+  private void runJMhBenchmarkAndLog(String operationName, BenchmarkAction action) throws IOException {
     String banner = "======================================================";
     String startMessage = String.format("JMH BENCHMARK TRIGGERED FOR %s OPERATION!", operationName);
     String endMessage = String.format("JMH BENCHMARK FINISHED FOR %s.", operationName);
@@ -54,14 +54,14 @@ public class GoogleHadoopFileSystemJMHBenchmarking extends GoogleHadoopFileSyste
 
   @Override
   public FileStatus[] listStatus(Path hadoopPath) throws IOException {
-    runBenchmarkAndLog("LISTSTATUS", () -> GCSListStatusBenchmark.runBenchmark(hadoopPath));
+    runJMhBenchmarkAndLog("LISTSTATUS", () -> GCSListStatusBenchmark.runBenchmark(hadoopPath));
     logger.atInfo().log("Benchmark complete. Now performing the actual 'listStatus' operation...");
     return super.listStatus(hadoopPath); // Run actual listStatus Operation after benchmarking it.
   }
 
   @Override
   public boolean rename(Path src, Path dst) throws IOException {
-    runBenchmarkAndLog("RENAME", () -> GCSRenameBenchmark.runBenchmark(src, dst));
+    runJMhBenchmarkAndLog("RENAME", () -> GCSRenameBenchmark.runBenchmark(src, dst));
     logger.atInfo().log("Benchmark complete. Now performing the actual 'rename' operation...");
     return super.rename(src, dst);
   }
@@ -69,7 +69,7 @@ public class GoogleHadoopFileSystemJMHBenchmarking extends GoogleHadoopFileSyste
   @Override
   public boolean delete(Path hadoopPath, boolean recursive) throws IOException {
     // Run the single-shot benchmark, which will delete the actual file/folder.
-    runBenchmarkAndLog("DELETE", () -> GCSDeleteBenchmark.runBenchmark(hadoopPath, recursive));
+    runJMhBenchmarkAndLog("DELETE", () -> GCSDeleteBenchmark.runBenchmark(hadoopPath, recursive));
 
     // IMPORTANT: Because the benchmark itself performs the final delete, we DO NOT call
     // super.delete() here. We simply return true to indicate the user's command succeeded.
