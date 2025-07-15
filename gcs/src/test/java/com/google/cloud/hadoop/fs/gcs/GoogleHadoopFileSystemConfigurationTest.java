a74139a56929bb0a37a3e4d66b563eabc6bfce9d
Dheeraj Singh <dheerajsinghh@google.com>
2025-07-16 00:50:01 +0530

    enable move by default

--- gcs/src/test/java/com/google/cloud/hadoop/fs/gcs/GoogleHadoopFileSystemConfigurationTest.java
+++ gcs/src/test/java/com/google/cloud/hadoop/fs/gcs/GoogleHadoopFileSystemConfigurationTest.java
@@ -131,13 +131,14 @@ public class GoogleHadoopFileSystemConfigurationTest {
               PartFileCleanupType.ALWAYS);
           put("fs.gs.write.parallel.composite.upload.part.file.name.prefix", "");
           put("fs.gs.fadvise.request.track.count", 3);
-          put("fs.gs.operation.move.enable", false);
+          put("fs.gs.operation.move.enable", true);
           put("fs.gs.write.rolling.checksum.enable", true);
           put("fs.gs.bidi.enable", false);
           put("fs.gs.bidi.thread.count", 16);
           put("fs.gs.bidi.client.timeout", 30);
           put("fs.gs.bidi.finalize.on.close", false);
           put("fs.gs.storage.client.cache.enable", false);
+          put("fs.gs.write.rolling.checksum.enable", false);
         }
       };
 
