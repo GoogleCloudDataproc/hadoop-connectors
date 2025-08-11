d5fa79c61557fa67a2cf374deb38727ead8adeb3
cpriti-os <cpriti@google.com>
2025-08-11 05:47:19 +0000

    Add a comment to explain thread safety

--- util/src/main/java/com/google/cloud/hadoop/util/interceptors/InvocationIdInterceptor.java
+++ util/src/main/java/com/google/cloud/hadoop/util/interceptors/InvocationIdInterceptor.java
@@ -65,7 +65,8 @@ public final class InvocationIdInterceptor implements HttpExecuteInterceptor {
     final String signatureKey = "Signature="; // For V2 and V4 signedURLs
     final String builtURL = request.getUrl().build();
     if (!builtURL.contains(signatureKey)) {
-      // Thread-safe: InvocationIdContext uses thread-local storage, so each thread has its own invocationId.
+      // Thread-safe: InvocationIdContext uses thread-local storage, so each thread has its own
+      // invocationId.
       if (InvocationIdContext.getInvocationId().isEmpty()) {
         InvocationIdContext.setInvocationId();
       }
