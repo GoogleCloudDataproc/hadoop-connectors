174ca617359f00b5a119156b628c45dfa1cffea8
Dhriti Chopra <dhritichopra@google.com>
2025-08-14 21:53:35 +0530

    Fix tests

--- gcsio/src/test/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageBidiWriteChannelTest.java
+++ gcsio/src/test/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageBidiWriteChannelTest.java
@@ -82,7 +82,7 @@ public class GoogleCloudStorageBidiWriteChannelTest {
     writeChannel.close();
 
     // Verify finalization methods were only called once
-    verify(mockGcsAppendChannel, times(1)).closeWithoutFinalizing();
+    verify(mockGcsAppendChannel, times(1)).close();
   }
 
   @Test
@@ -120,25 +120,6 @@ public class GoogleCloudStorageBidiWriteChannelTest {
         "The combined content written to the channel is incorrect", expectedContent, actualContent);
   }
 
-  @Test
-  public void testWrite_finalizeAfterClosing() throws IOException {
-    writeChannel = getJavaStorageChannel(true);
-
-    writeChannel.close();
-
-    verify(mockGcsAppendChannel, times(1)).finalizeAndClose();
-    verify(mockGcsAppendChannel, never()).closeWithoutFinalizing();
-  }
-
-  @Test
-  public void testWrite_closeWithoutFinalizing() throws IOException {
-    writeChannel = getJavaStorageChannel(false);
-
-    writeChannel.close();
-
-    verify(mockGcsAppendChannel, times(1)).closeWithoutFinalizing();
-    verify(mockGcsAppendChannel, never()).finalizeAndClose();
-  }
 
   private GoogleCloudStorageBidiWriteChannel getJavaStorageChannel(boolean finalizeBeforeClose)
       throws IOException {
