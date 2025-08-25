ef024ec77b55d10587e192b1fe28f4e6e99e6b3f
Dhriti Chopra <dhritichopra@google.com>
2025-08-25 14:02:38 +0530

    Adding Read functionality for bidi Read channel

--- gcsio/src/main/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageBidiReadChannel.java
+++ gcsio/src/main/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageBidiReadChannel.java
@@ -1,25 +1,28 @@
 package com.google.cloud.hadoop.gcsio;
 
+import static com.google.common.base.Strings.nullToEmpty;
+
 import com.google.api.core.ApiFuture;
 import com.google.api.core.ApiFutureCallback;
 import com.google.api.core.ApiFutures;
-import com.google.cloud.storage.BlobId;
-import com.google.cloud.storage.BlobReadSession;
-import com.google.cloud.storage.RangeSpec;
-import com.google.cloud.storage.ReadProjectionConfigs;
-import com.google.cloud.storage.Storage;
+import com.google.cloud.hadoop.util.GoogleCloudStorageEventBus;
+import com.google.cloud.storage.*;
 import com.google.cloud.storage.ZeroCopySupport.DisposableByteString;
 import com.google.common.flogger.GoogleLogger;
 import com.google.protobuf.ByteString;
+import java.io.EOFException;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.channels.ClosedChannelException;
 import java.nio.channels.SeekableByteChannel;
+import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.function.IntFunction;
+import javax.annotation.Nullable;
 
 public class GoogleCloudStorageBidiReadChannel implements ReadVectoredSeekableByteChannel {
 
@@ -29,7 +32,15 @@ public class GoogleCloudStorageBidiReadChannel implements ReadVectoredSeekableBy
   private final BlobId blobId;
   private final GoogleCloudStorageReadOptions readOptions;
   private final BlobReadSession blobReadSession;
-  private ExecutorService boundedThreadPool;
+  private final ExecutorService boundedThreadPool;
+  private static final String GZIP_ENCODING = "gzip";
+  private long objectSize;
+  private boolean isOpen = true;
+  private boolean gzipEncoded = false;
+  private final StorageResourceId resourceId;
+
+  private final Duration readTimeout;
+  private long position = 0;
 
   public GoogleCloudStorageBidiReadChannel(
       Storage storage,
@@ -37,17 +48,17 @@ public class GoogleCloudStorageBidiReadChannel implements ReadVectoredSeekableBy
       GoogleCloudStorageReadOptions readOptions,
       ExecutorService boundedThreadPool)
       throws IOException {
-    this.storage = storage;
+    this.readTimeout = readOptions.getGrpcReadTimeout();
     this.resourceId =
         new StorageResourceId(
             itemInfo.getBucketName(), itemInfo.getObjectName(), itemInfo.getContentGeneration());
-    this.blobId =
+    BlobId blobId =
         BlobId.of(
             resourceId.getBucketName(), resourceId.getObjectName(), resourceId.getGenerationId());
-    this.readOptions = readOptions;
     this.blobReadSession =
         initializeBlobReadSession(storage, blobId, readOptions.getBidiClientTimeout());
     this.boundedThreadPool = boundedThreadPool;
+    initMetadata(itemInfo.getContentEncoding(), itemInfo.getSize());
   }
 
   private static BlobReadSession initializeBlobReadSession(
@@ -60,6 +71,7 @@ public class GoogleCloudStorageBidiReadChannel implements ReadVectoredSeekableBy
           "Client Initialization successful in %d ms.",
           System.currentTimeMillis() - clientInitializationDurationStartTime);
     } catch (InterruptedException | ExecutionException | TimeoutException e) {
+      GoogleCloudStorageEventBus.postOnException();
       throw new IOException(e);
     }
     return readSession;
@@ -67,48 +79,111 @@ public class GoogleCloudStorageBidiReadChannel implements ReadVectoredSeekableBy
 
   @Override
   public int read(ByteBuffer dst) throws IOException {
-    // TODO(dhritichopra) Add read flow
-    return 0;
-  }
+    throwIfNotOpen();
+    if (!dst.hasRemaining()) {
+      return 0;
+    }
 
-  @Override
-  public int write(ByteBuffer src) throws IOException {
-    throw new UnsupportedOperationException("Cannot mutate read-only channel");
+    if (position >= objectSize) {
+      return -1;
+    }
+
+    logger.atFinest().log(
+        "Reading up to %d bytes at position %d from '%s'", dst.remaining(), position, resourceId);
+
+    long bytesToRequest = Math.min(dst.remaining(), objectSize - position);
+
+    try {
+      ApiFuture<DisposableByteString> futureBytes =
+          blobReadSession.readAs(
+              ReadProjectionConfigs.asFutureByteString()
+                  .withRangeSpec(RangeSpec.of(position, bytesToRequest)));
+
+      DisposableByteString disposableByteString =
+          futureBytes.get(readTimeout.toNanos(), TimeUnit.NANOSECONDS);
+
+      int bytesRead;
+      try (DisposableByteString dbs = disposableByteString) {
+        ByteString byteString = dbs.byteString();
+        bytesRead = byteString.size();
+
+        if (bytesRead == 0 && position < objectSize) {
+          throw new IOException(
+              String.format("Read 0 bytes without blocking from object: '%s'", resourceId));
+        }
+
+        if (bytesRead > 0) {
+          byteString.copyTo(dst);
+          position += bytesRead;
+        }
+      }
+
+      return bytesRead > 0 ? bytesRead : -1;
+
+    } catch (InterruptedException | ExecutionException | TimeoutException e) {
+      GoogleCloudStorageEventBus.postOnException();
+      throw new IOException(
+          String.format("Read failed on %s at position %d", resourceId, position), e);
+    }
   }
 
   @Override
   public long position() throws IOException {
-    // TODO(dhritichopra) Add read flow
-    return 0;
+    throwIfNotOpen();
+    return position;
   }
 
   @Override
   public SeekableByteChannel position(long newPosition) throws IOException {
-    // TODO(dhritichopra) Add read flow
-    return null;
+    throwIfNotOpen();
+    validatePosition(newPosition);
+    if (newPosition == this.position) {
+      return this;
+    }
+
+    logger.atFinest().log(
+        "Seek from %d to %d position for '%s'", this.position, newPosition, resourceId);
+
+    this.position = newPosition;
+    return this;
+  }
+
+  @Override
+  public int write(ByteBuffer src) throws IOException {
+    GoogleCloudStorageEventBus.postOnException();
+    throw new UnsupportedOperationException("Cannot mutate read-only channel");
   }
 
   @Override
   public long size() throws IOException {
-    // TODO(dhritichopra) Add read flow
-    return 0;
+    throwIfNotOpen();
+    if (objectSize == -1) {
+      GoogleCloudStorageEventBus.postOnException();
+      throw new IOException("Size of file is not available");
+    }
+    return objectSize;
   }
 
   @Override
   public SeekableByteChannel truncate(long size) throws IOException {
-    // TODO(dhritichopra) Add read flow
-    return null;
+    GoogleCloudStorageEventBus.postOnException();
+    throw new UnsupportedOperationException("Cannot truncate a read-only channel");
   }
 
   @Override
   public boolean isOpen() {
-    // TODO(dhritichopra) Add read flow
-    return false;
+    return isOpen;
   }
 
   @Override
   public void close() throws IOException {
-    blobReadSession.close();
+    if (isOpen) {
+      isOpen = false;
+      logger.atFinest().log("Closing channel for '%s'", resourceId);
+      if (blobReadSession != null) {
+        blobReadSession.close();
+      }
+    }
   }
 
   @Override
@@ -168,13 +243,47 @@ public class GoogleCloudStorageBidiReadChannel implements ReadVectoredSeekableBy
       int size = byteString.size();
       bytesRead += size;
       ByteBuffer buf = allocate.apply(size);
-      // This loop efficiently copies data without creating intermediate byte arrays on the heap
       for (ByteBuffer b : byteString.asReadOnlyByteBufferList()) {
         buf.put(b);
       }
-      buf.flip(); // Prepare buffer for reading
+      buf.flip();
       range.getData().complete(buf);
     }
     return bytesRead;
+  }
+
+  private void throwIfNotOpen() throws IOException {
+    if (!isOpen()) {
+      GoogleCloudStorageEventBus.postOnException();
+      throw new ClosedChannelException();
+    }
+  }
+
+  protected void initMetadata(@Nullable String encoding, long sizeFromMetadata)
+      throws UnsupportedOperationException {
+    gzipEncoded = nullToEmpty(encoding).contains(GZIP_ENCODING);
+    if (gzipEncoded) {
+      GoogleCloudStorageEventBus.postOnException();
+      throw new UnsupportedOperationException("Gzip Encoded Files are not supported");
+    }
+    objectSize = sizeFromMetadata;
+  }
+
+  private void validatePosition(long position) throws IOException {
+    if (position < 0) {
+      GoogleCloudStorageEventBus.postOnException();
+      throw new EOFException(
+          String.format(
+              "Invalid seek offset: position value (%d) must be >= 0 for '%s'",
+              position, resourceId));
+    }
+
+    if (objectSize >= 0 && position >= objectSize) {
+      GoogleCloudStorageEventBus.postOnException();
+      throw new EOFException(
+          String.format(
+              "Invalid seek offset: position value (%d) must be between 0 and %d for '%s'",
+              position, objectSize, resourceId));
+    }
   }
 }
