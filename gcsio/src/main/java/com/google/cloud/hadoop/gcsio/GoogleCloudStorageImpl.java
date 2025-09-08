0d5bdea917a9276af4cc194ea439e62fd4dd7b2e
Claire McGinty <claire.d.mcginty@gmail.com>
2025-09-08 14:37:11 -0400

    Remove httpRequestInitializer override warning from GoogleCloudStorageImpl

--- gcsio/src/main/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageImpl.java
+++ gcsio/src/main/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageImpl.java
@@ -325,8 +325,6 @@ public class GoogleCloudStorageImpl implements GoogleCloudStorage {
       finalHttpRequestInitializer =
           new RetryHttpInitializer(finalCredentials, options.toRetryHttpInitializerOptions());
     } else {
-      logger.atWarning().log(
-          "ALERT: Overriding httpRequestInitializer - this should not be done in production!");
       finalHttpRequestInitializer = httpRequestInitializer;
     }
     this.httpRequestInitializer =
