f33f12a0a6c4308e159c854a22c807a8c530d704
dheerajsngh <dheerajsinghh@google.com>
2025-09-11 18:32:25 +0530

    Remove `testRenameWithMoveOperation` from the list of ignored tests for gRPC client. (#1502)

--- gcs/src/test/java/com/google/cloud/hadoop/fs/gcs/GoogleHadoopFileSystemJavaStorageClientIntegrationTest.java
+++ gcs/src/test/java/com/google/cloud/hadoop/fs/gcs/GoogleHadoopFileSystemJavaStorageClientIntegrationTest.java
@@ -64,10 +64,6 @@ public class GoogleHadoopFileSystemJavaStorageClientIntegrationTest
 
   @Ignore
   @Test
-  public void testRenameWithMoveOperation() {}
-
-  @Ignore
-  @Test
   public void testGcsJsonAPIMetrics() {
     // TODO: Update this will once gRPC API metrics are added
   }
