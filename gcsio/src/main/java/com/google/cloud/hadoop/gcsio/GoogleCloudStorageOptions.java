a3d2fd9587f3642187bbe6d317bc8bf3941ae144
dheerajsngh <dheerajsinghh@google.com>
2025-09-15 21:40:05 +0530

    Enable move by default (#1417)

--- gcsio/src/main/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageOptions.java
+++ gcsio/src/main/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageOptions.java
@@ -67,7 +67,7 @@ public abstract class GoogleCloudStorageOptions {
         .setWriteChannelOptions(AsyncWriteChannelOptions.DEFAULT)
         .setHnBucketRenameEnabled(false)
         .setGrpcWriteEnabled(false)
-        .setMoveOperationEnabled(false);
+        .setMoveOperationEnabled(true);
   }
 
   public abstract Builder toBuilder();
