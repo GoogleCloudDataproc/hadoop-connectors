19bf83ae264cae07681b4812419a3512262577b9
Dheeraj Singh <dheerajsinghh@google.com>
2025-09-16 18:13:22 +0530

    Fixed failing test

--- gcsio/src/main/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageFileSystemImpl.java
+++ gcsio/src/main/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageFileSystemImpl.java
@@ -1083,13 +1083,6 @@ public class GoogleCloudStorageFileSystemImpl implements GoogleCloudStorageFileS
     return getFileInfoInternal(resourceId, inferImplicitDirectories, PathTypeHint.NONE);
   }
 
-  private boolean isHnsOptimized(URI path) throws IOException {
-    GoogleCloudStorageOptions gcsOptions = options.getCloudStorageOptions();
-    return gcsOptions.isHnBucketRenameEnabled()
-        && gcsOptions.isHnOptimizationEnabled()
-        && gcs.isHnBucket(path);
-  }
-
   /**
    * @see #getFileInfo(URI)
    */
