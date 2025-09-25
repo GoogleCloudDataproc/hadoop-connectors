a4d10a16371e1712d4d8aa94f5609247d963cf82
Dheeraj Singh <dheerajsinghh@google.com>
2025-09-25 12:21:33 +0530

    maven spotless fix

--- gcs/src/test/java/com/google/cloud/hadoop/fs/gcs/CoopLockRepairIntegrationTest.java
+++ gcs/src/test/java/com/google/cloud/hadoop/fs/gcs/CoopLockRepairIntegrationTest.java
@@ -91,10 +91,12 @@ public class CoopLockRepairIntegrationTest {
     Credential credential =
         checkNotNull(GoogleCloudStorageTestHelper.getCredential(), "credential must not be null");
 
-    gcsOptions = getStandardOptionBuilder().
-                 //disable move as the written here are using copy + delete
-                 setMoveOperationEnabled(false)
-                .build();
+    gcsOptions =
+        getStandardOptionBuilder()
+            .
+            // disable move as the written here are using copy + delete
+            setMoveOperationEnabled(false)
+            .build();
     httpRequestInitializer =
         new RetryHttpInitializer(credential, gcsOptions.toRetryHttpInitializerOptions());
 
