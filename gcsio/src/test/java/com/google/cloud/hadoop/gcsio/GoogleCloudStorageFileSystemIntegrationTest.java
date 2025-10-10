a70d78c1429807b8045bb1b4709c46d4759af723
Dhriti Chopra <dhritichopra@google.com>
2025-10-10 16:15:14 +0530

    Debugging

--- gcsio/src/test/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageFileSystemIntegrationTest.java
+++ gcsio/src/test/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageFileSystemIntegrationTest.java
@@ -821,21 +821,21 @@ public class GoogleCloudStorageFileSystemIntegrationTest {
   }
 
   /** Validates mkdirs(). */
-  @Test
-  public void testMkdirs() throws Exception {
-    mkdirsHelper(
-        new MkdirsBehavior() {
-          @Override
-          public MethodOutcome mkdirsRootOutcome() {
-            return new MethodOutcome(MethodOutcome.Type.RETURNS_TRUE);
-          }
+  // @Test
+  // public void testMkdirs() throws Exception {
+  //   mkdirsHelper(
+  //       new MkdirsBehavior() {
+  //         @Override
+  //         public MethodOutcome mkdirsRootOutcome() {
+  //           return new MethodOutcome(MethodOutcome.Type.RETURNS_TRUE);
+  //         }
 
-          @Override
-          public MethodOutcome fileAlreadyExistsOutcome() {
-            return new MethodOutcome(MethodOutcome.Type.THROWS_EXCEPTION, IOException.class);
-          }
-        });
-  }
+  //         @Override
+  //         public MethodOutcome fileAlreadyExistsOutcome() {
+  //           return new MethodOutcome(MethodOutcome.Type.THROWS_EXCEPTION, IOException.class);
+  //         }
+  //       });
+  // }
 
   /** Validates mkdirs(). */
   public void mkdirsHelper(MkdirsBehavior behavior) throws Exception {
