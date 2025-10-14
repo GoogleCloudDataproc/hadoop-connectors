cfb671ac55959373ac2c082e7020c968dc3c49c0
Dhriti Chopra <dhritichopra@google.com>
2025-10-14 14:15:00 +0530

    Formatting

--- gcsio/src/test/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageBidiWriteChannelTest.java
+++ gcsio/src/test/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageBidiWriteChannelTest.java
@@ -33,9 +33,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.mockito.stubbing.Answer;
 
 @RunWith(MockitoJUnitRunner.class)
 public class GoogleCloudStorageBidiWriteChannelTest {
