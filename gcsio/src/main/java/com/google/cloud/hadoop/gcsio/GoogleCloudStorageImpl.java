7431fa1eedaea8e7097f4573cba831a5ece71431
dheerajsngh <dheerajsinghh@google.com>
2026-02-19 11:40:52 +0530

    [Backport 3.0.x] Backport List retry fix (#1643)

--- gcsio/src/main/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageImpl.java
+++ gcsio/src/main/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageImpl.java
@@ -1575,8 +1575,28 @@ public class GoogleCloudStorageImpl implements GoogleCloudStorage {
     try (ITraceOperation op =
         TraceOperation.addToExistingTrace(
             String.format("gcs.objects.list(page=%s)", pageContext))) {
-      items = listObject.execute();
+      items =
+          ResilientOperation.retry(
+              listObject::execute,
+              backOffFactory.newBackOff(),
+              (e) -> {
+                // Retry on Rate Limit
+                if (errorExtractor.rateLimited(e)) {
+                  return true;
+                }
+                // Retry on Network Errors (SocketException)
+                // Ignore 4xx errors (GoogleJsonResponseException)
+                if (e instanceof IOException && !(e instanceof GoogleJsonResponseException)) {
+                  return true;
+                }
+                return false;
+              },
+              IOException.class,
+              sleeper);
       op.annotate("resultSize", items == null ? 0 : items.size());
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+      throw new IOException("Thread interrupted while listing objects", e);
     } catch (IOException e) {
       GoogleCloudStorageEventBus.postOnException();
       String resource = StringPaths.fromComponents(listObject.getBucket(), listObject.getPrefix());
