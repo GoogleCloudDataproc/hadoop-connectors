6a97855d1784b97a2afeab8a80cb8655f2ecd36a
Ravi Singh <rdsingh@google.com>
2026-07-06 06:17:30 +0000

    added NPE check

--- gcs/src/main/java/com/google/cloud/hadoop/fs/gcs/GoogleHadoopFileSystem.java
+++ gcs/src/main/java/com/google/cloud/hadoop/fs/gcs/GoogleHadoopFileSystem.java
@@ -1410,7 +1410,9 @@ public class GoogleHadoopFileSystem extends FileSystem implements IOStatisticsSo
         return true;
       case GcsConnectorCapabilities.LIST_STATUS_STARTING_FROM:
         try {
-          if (isClosed()) {
+          // getGcs should not be null, there is no way we close the underneath gcs and not close hcfs.
+          // still added this check to avoid any NPE
+          if (isClosed() || getGcsFs().getGcs() == null) {
             return false;
           }
           return !getGcsFs().getGcs().isHnBucket(getGcsPath(path));
