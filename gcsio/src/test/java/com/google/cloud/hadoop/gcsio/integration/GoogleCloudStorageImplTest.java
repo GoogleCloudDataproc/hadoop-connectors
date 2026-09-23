74a2da7ef6e572a604f8d0ba83c0381cf0e5d02f
Shruti Singhania <singhaniash@google.com>
2026-09-23 10:33:44 +0530

    ensure cleanup of created objects and update naming prefix in GoogleCloudStorageImplTest

--- gcsio/src/test/java/com/google/cloud/hadoop/gcsio/integration/GoogleCloudStorageImplTest.java
+++ gcsio/src/test/java/com/google/cloud/hadoop/gcsio/integration/GoogleCloudStorageImplTest.java
@@ -629,7 +629,7 @@ public class GoogleCloudStorageImplTest {
 
   @Test
   public void listObjectInfoStartingFrom_lexicographicalOrdrer() throws IOException {
-    String testDirectory = name.getMethodName();
+    String testDirectory = "~" + name.getMethodName();
 
     TrackingStorageWrapper<GoogleCloudStorage> trackingGcs =
         newTrackingGoogleCloudStorage(GCS_OPTIONS);
@@ -637,24 +637,28 @@ public class GoogleCloudStorageImplTest {
     StorageResourceId resourceId2 = new StorageResourceId(testBucket, testDirectory + "/object2");
     StorageResourceId resourceId1 = new StorageResourceId(testBucket, testDirectory + "/object1");
 
-    trackingGcs.delegate.createEmptyObject(resourceId3);
-    trackingGcs.delegate.createEmptyObject(resourceId2);
-    trackingGcs.delegate.createEmptyObject(resourceId1);
+    try {
+      trackingGcs.delegate.createEmptyObject(resourceId3);
+      trackingGcs.delegate.createEmptyObject(resourceId2);
+      trackingGcs.delegate.createEmptyObject(resourceId1);
 
-    // Verify that directory object not listed
-    List<GoogleCloudStorageItemInfo> listedItems =
-        helperGcs.listObjectInfoStartingFrom(testBucket, testDirectory + "/");
-    assertThat(listedItems.stream().map(GoogleCloudStorageItemInfo::getResourceId).toArray())
-        .asList()
-        .containsExactlyElementsIn(
-            ImmutableList.builder()
-                .add(resourceId1)
-                .add(resourceId2)
-                .add(resourceId3)
-                .build()
-                .toArray())
-        .inOrder();
-    trackingGcs.delegate.close();
+      // Verify that directory object not listed
+      List<GoogleCloudStorageItemInfo> listedItems =
+          helperGcs.listObjectInfoStartingFrom(testBucket, testDirectory + "/");
+      assertThat(listedItems.stream().map(GoogleCloudStorageItemInfo::getResourceId).toArray())
+          .asList()
+          .containsExactlyElementsIn(
+              ImmutableList.builder()
+                  .add(resourceId1)
+                  .add(resourceId2)
+                  .add(resourceId3)
+                  .build()
+                  .toArray())
+          .inOrder();
+    } finally {
+      helperGcs.deleteObjects(ImmutableList.of(resourceId1, resourceId2, resourceId3));
+      trackingGcs.delegate.close();
+    }
   }
 
   @Test
